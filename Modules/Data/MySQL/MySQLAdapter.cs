35e2eb307264dff817d516f8b2fea3b1d38a2ff4
Leo Botinelly <lmbs001@bucknell.edu>
2015-10-16 18:41:05 -0400

    Stub push
    Placeholders for Adapters

---
+++ Modules/Data/MySQL/MySQLAdapter.cs
@@ -0,0 +1,65 @@
+﻿using Nyan.Core.Modules.Data.Adapter;
+using System;
+using System.Data.Common;
+
+namespace Nyan.Modules.Data.MySQL
+{
+    public class MySQLAdapter : AdapterPrimitive
+    {
+        public override string ParameterIdentifier
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override bool UseOutputParameterForInsertedKeyExtraction
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public override void CheckDatabaseEntities<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void ClearPools()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DbConnection Connection(string connectionString)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DynamicParametersPrimitive InsertableParameters<T>(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override DynamicParametersPrimitive Parameters<T>(object obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void RenderSchemaEntityNames<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetConnectionString<T>()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetSqlStatements<T>()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Modules/Data/MySQL/MySQLDynamicParameters.cs
@@ -0,0 +1,15 @@
+﻿using Dapper;
+using Nyan.Core.Modules.Data.Adapter;
+using System;
+using System.Data;
+
+namespace Nyan.Modules.Data.MySQL
+{
+    public class MySQLDynamicParameters : DynamicParametersPrimitive, SqlMapper.IDynamicParameters
+    {
+        public override void AddParameters(IDbCommand command, SqlMapper.Identity identity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
