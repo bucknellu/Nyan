05043b42361ddc1cfb237efeaa60ea621f6a31bb
lbotinelly <lmbs001@bucknell.edu>
2015-10-30 14:08:47 -0400

    Functional Redis cache engine.

---
+++ Modules/Cache/Redis/RedisCacheConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using Nyan.Core.Modules.Cache;
+
+namespace Nyan.Modules.Cache.Redis
+{
+    public class RedisCacheConfiguration : ICacheConfiguration
+    {
+        public string ConnectionString { get; set; }
+        public int DatabaseIndex { get; set; }
+    }
+}
--- Modules/Cache/Redis/RedisCacheProvider.cs
+++ Modules/Cache/Redis/RedisCacheProvider.cs
@@ -1,60 +1,204 @@
-﻿using Nyan.Core.Modules.Cache;
+﻿using System.Linq;
+using Nyan.Core.Extensions;
+using Nyan.Core.Modules.Cache;
 using System;
 using System.Collections.Generic;
+using Nyan.Core.Modules.Log;
+using StackExchange.Redis;
 
 namespace Nyan.Modules.Cache.Redis
 {
     public class RedisCacheProvider : ICacheProvider
     {
+        public Dictionary<string, ICacheConfiguration> ScopeConfiguration { get; set; }
+
+        private static ConnectionMultiplexer _redis;
+
+        private static string _currentServer = "none";
+        private static int _dbIdx = -1;
+
+
+        #region custom implementation
+        private static int DatabaseIndex
+        {
+            get { return _dbIdx; }
+        }
+
+        public void Initialize()
+        {
+            //In the case nothing is defined, a standard environment setup is provided.
+            if (ScopeConfiguration == null)
+                ScopeConfiguration = new Dictionary<string, ICacheConfiguration>
+                {
+                    {"STD", new RedisCacheConfiguration {DatabaseIndex = 5, ConnectionString = "localhost"}}
+                };
+
+
+            var probe = (RedisCacheConfiguration)ScopeConfiguration[Core.Settings.Current.Scope.CurrentCode];
+            _dbIdx = probe.DatabaseIndex;
+            _currentServer = probe.ConnectionString;
+
+            Connect();
+        }
+
+        public void Connect()
+        {
+            try
+            {
+                ServerName = _currentServer.Split(',')[0];
+
+                //The connection string may be encrypted. Try to decrypt it, but ignore if it fails.
+                try { _currentServer = Core.Settings.Current.Encryption.Decrypt(_currentServer); }
+                catch { }
+
+                _redis = ConnectionMultiplexer.Connect(_currentServer);
+                OperationalStatus = EOperationalStatus.Operational;
+            }
+            catch (Exception e)
+            {
+                OperationalStatus = EOperationalStatus.NonOperational;
+                Core.Settings.Current.Log.Add("REDIS unavailable: Application running on direct database mode.", Message.EContentType.Maintenance);
+                Core.Settings.Current.Log.Add(e);
+            }
+        }
+
+        #endregion
+
         public string this[string key, string oSet = null, int cacheTimeOutSeconds = 600]
         {
             get
             {
-                throw new NotImplementedException();
+                if (OperationalStatus != EOperationalStatus.Operational) return null;
+                try
+                {
+                    var db = _redis.GetDatabase(DatabaseIndex);
+                    return db.StringGet(key);
+                }
+                catch (Exception)
+                {
+                    OperationalStatus = EOperationalStatus.Error;
+                    throw;
+                }
             }
-
             set
             {
-                throw new NotImplementedException();
+                if (OperationalStatus != EOperationalStatus.Operational) return;
+
+                try
+                {
+                    var db = _redis.GetDatabase(DatabaseIndex);
+                    if (cacheTimeOutSeconds == 0)
+                        db.StringSet(key, value);
+                    else
+                        db.StringSet(key, value, TimeSpan.FromSeconds(cacheTimeOutSeconds));
+
+                    //if (oSet != null)
+                    //    db.SetAdd(oSet, value);
+                }
+                catch (Exception)
+                {
+                    OperationalStatus = EOperationalStatus.Error;
+                    throw;
+                }
             }
         }
 
-        public EOperationalStatus OperationalStatus
-        {
-            get
-            {
-                throw new NotImplementedException();
-            }
-        }
+        public string ServerName { get; private set; }
+
+        public EOperationalStatus OperationalStatus { get; set; }
 
         public bool Contains(string key)
         {
-            throw new NotImplementedException();
+            if (OperationalStatus != EOperationalStatus.Operational) return false;
+
+            try
+            {
+                var db = _redis.GetDatabase(DatabaseIndex);
+                return db.KeyExists(key);
+            }
+            catch (Exception)
+            {
+                OperationalStatus = EOperationalStatus.Error;
+                throw;
+            }
         }
 
         public IEnumerable<string> GetAll(string oNamespace)
         {
-            throw new NotImplementedException();
+            if (OperationalStatus != EOperationalStatus.Operational) return null;
+
+            try
+            {
+                var db = _redis.GetDatabase(DatabaseIndex);
+                var conn = _redis.GetEndPoints()[0];
+                var svr = _redis.GetServer(conn);
+                var keys = svr.Keys(pattern: oNamespace + "*").ToList();
+
+                var ret = keys.Select(a => db.StringGet(a).ToString()).ToList();
+                return ret;
+
+                //return db.SetMembers(oNamespace).Select(i => i.ToString()).ToList();
+            }
+            catch (Exception)
+            {
+                OperationalStatus = EOperationalStatus.Error;
+                throw;
+            }
         }
 
         public T GetSingleton<T>(string fullName = null)
         {
-            throw new NotImplementedException();
+            var n = (fullName ?? typeof(T).FullName) + ":s";
+            var c = this[n];
+
+            return c == null ? default(T) : c.FromJson<T>();
         }
 
         public void Remove(string key, string oSet = null)
         {
-            throw new NotImplementedException();
+            if (OperationalStatus != EOperationalStatus.Operational) return;
+
+            try
+            {
+                var db = _redis.GetDatabase(DatabaseIndex);
+                //if (oSet != null)
+                //db.SetRemove(oSet, this[key]);
+
+                db.KeyDelete(key);
+            }
+            catch (Exception)
+            {
+                OperationalStatus = EOperationalStatus.Error;
+                throw;
+            }
         }
 
         public void RemoveAll(string oSet = null)
         {
-            throw new NotImplementedException();
+            if (OperationalStatus != EOperationalStatus.Operational) return;
+
+            foreach (var endPoint in _redis.GetEndPoints())
+            {
+                var server = _redis.GetServer(endPoint);
+
+                var db = _redis.GetDatabase(DatabaseIndex);
+
+                var keys = server.Keys();
+                foreach (var key in keys)
+                {
+                    db.KeyDelete(key);
+                }
+
+                //server.FlushDatabase(DatabaseIndex);
+            }
         }
 
         public void SetSingleton(object value, string fullName = null)
         {
-            throw new NotImplementedException();
+            if (OperationalStatus != EOperationalStatus.Operational) return;
+
+            var n = (fullName ?? value.GetType().FullName) + ":s";
+            this[n] = value.ToJson();
         }
     }
 }
