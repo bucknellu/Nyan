ab0e1e39ffdee32bff2f9eeb742fc6c1ee030c99
lbotinelly <lmbs001@bucknell.edu>
2015-11-04 15:56:16 -0500

    Cosmetics

--- Modules/Log/ZeroMQ/ZeroMqLogProvider.cs
+++ Modules/Log/ZeroMQ/ZeroMqLogProvider.cs
@@ -1,33 +1,31 @@
 ﻿using System;
-using System.ComponentModel;
 using System.Diagnostics;
-using Nyan.Core.Extensions;
 using Nyan.Core.Modules.Log;
 
 namespace Nyan.Modules.Log.ZeroMQ
 {
     public class ZeroMqLogProvider : LogProvider
     {
-        public new event Message.MessageArrivedHandler MessageArrived;
-
-        private Channel _out;
         private Channel _in;
         private string _multiCastAddress = "";
+        private Channel _out;
 
         public ZeroMqLogProvider()
         {
             Initialize("pgm://239.255.42.99:5558");
         }
 
+        public ZeroMqLogProvider(string multiCastAddress)
+        {
+            Initialize(multiCastAddress);
+        }
+
+        public new event Message.MessageArrivedHandler MessageArrived;
+
         private void Initialize(string multiCastAddress)
         {
             _multiCastAddress = multiCastAddress;
             _out = new Channel("Log_Service", true, false, _multiCastAddress);
-        }
-
-        public ZeroMqLogProvider(string multiCastAddress)
-        {
-            Initialize(multiCastAddress);
         }
 
         public override void Dispatch(string content, Message.EContentType type = Message.EContentType.Generic)
@@ -38,7 +36,7 @@ namespace Nyan.Modules.Log.ZeroMQ
                 Debug.WriteLine(content);
             }
 
-            var payload = new Message { Content = content, Subject = type.ToString(), Type = type };
+            var payload = new Message {Content = content, Subject = type.ToString(), Type = type};
 
             _out.Send(payload);
         }
