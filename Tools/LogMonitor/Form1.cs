b868274532d3ccae1631218e234d0c94218e7d01
lbotinelly <lmbs001@bucknell.edu>
2015-11-04 15:32:28 -0500

    Stub: log monitor tool

---
+++ Tools/LogMonitor/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nyan.Tools.LogMonitor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Nyan.Core.Settings.Current.Log.MessageArrived += Log_MessageArrived;
+            Nyan.Core.Settings.Current.Log.StartListening();
+        }
+
+        void Log_MessageArrived(Core.Modules.Log.Message message)
+        {
+            this.Text = message.Content;
+        }
+
+    }
+}
