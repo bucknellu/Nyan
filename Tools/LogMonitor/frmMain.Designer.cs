894ea264241d242f224ff2f32f3e3024c3a66c8c
Leo Botinelly <lmbs001@bucknell.edu>
2015-11-08 01:11:23 -0500

    ZeroMq default reference

--- Tools/LogMonitor/frmMain.Designer.cs
+++ Tools/LogMonitor/frmMain.Designer.cs
@@ -35,10 +35,10 @@
             this.colDescr = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tmrMaintenance = new System.Windows.Forms.Timer(this.components);
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnClear = new System.Windows.Forms.Button();
             this.chkIgGen = new System.Windows.Forms.CheckBox();
             this.btnAdjustColumns = new System.Windows.Forms.Button();
             this.chkAutoUpd = new System.Windows.Forms.CheckBox();
-            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -88,7 +88,7 @@
             this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.panel1.BackColor = System.Drawing.SystemColors.Info;
-            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.btnClear);
             this.panel1.Controls.Add(this.chkIgGen);
             this.panel1.Controls.Add(this.btnAdjustColumns);
             this.panel1.Controls.Add(this.chkAutoUpd);
@@ -96,6 +96,18 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(971, 29);
             this.panel1.TabIndex = 1;
+            // 
+            // btnClear
+            // 
+            this.btnClear.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnClear.Location = new System.Drawing.Point(334, 3);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(53, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = false;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
             // 
             // chkIgGen
             // 
@@ -130,18 +142,6 @@
             this.chkAutoUpd.Text = "Auto Update";
             this.chkAutoUpd.UseVisualStyleBackColor = true;
             // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Location = new System.Drawing.Point(334, 3);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(53, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "Clear";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -170,7 +170,7 @@
         private System.Windows.Forms.CheckBox chkAutoUpd;
         private System.Windows.Forms.CheckBox chkIgGen;
         private System.Windows.Forms.Button btnAdjustColumns;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnClear;
     }
 }
 
