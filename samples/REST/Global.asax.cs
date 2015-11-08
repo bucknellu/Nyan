2c492ad692e24c7e17ed3962bd78028b03384f61
Leo Botinelly <lmbs001@bucknell.edu>
2015-11-08 00:59:48 -0500

    Added Shutdown reason log entry

--- samples/REST/Global.asax.cs
+++ samples/REST/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Nyan.Core.Extensions;
+using System;
+using System.Reflection;
+using System.Web;
 using System.Web.Http;
 
 namespace Nyan.Samples.REST
@@ -8,6 +11,42 @@ namespace Nyan.Samples.REST
         protected void Application_Start(object sender, EventArgs e)
         {
             GlobalConfiguration.Configure(Nyan.Modules.Web.REST.Initialization.Register);
+            Nyan.Core.Settings.Current.Log.Add("Start!");
         }
+
+        public void Application_End()
+        {
+
+            HttpRuntime runtime = (HttpRuntime)typeof(System.Web.HttpRuntime).InvokeMember("_theRuntime",
+                                                                                            BindingFlags.NonPublic
+                                                                                            | BindingFlags.Static
+                                                                                            | BindingFlags.GetField,
+                                                                                            null,
+                                                                                            null,
+                                                                                            null);
+
+            if (runtime == null)
+                return;
+
+            string shutDownMessage = (string)runtime.GetType().InvokeMember("_shutDownMessage",
+                                                                             BindingFlags.NonPublic
+                                                                             | BindingFlags.Instance
+                                                                             | BindingFlags.GetField,
+                                                                             null,
+                                                                             runtime,
+                                                                             null);
+
+            string shutDownStack = (string)runtime.GetType().InvokeMember("_shutDownStack",
+                                                                           BindingFlags.NonPublic
+                                                                           | BindingFlags.Instance
+                                                                           | BindingFlags.GetField,
+                                                                           null,
+                                                                           runtime,
+                                                                           null);
+
+
+            Nyan.Core.Settings.Current.Log.Add("Shutting down: {0} : {1}".format(shutDownMessage, shutDownStack));
+        }
+
     }
 }
