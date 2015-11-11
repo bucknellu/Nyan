21c8a26b1ac0f186133b2f65c0a9dcbd067528e5
Leo Botinelly <lmbs001@bucknell.edu>
2015-11-10 23:28:46 -0500

    Added Route tracker to RouteProvider

--- Modules/Web/REST/CustomDirectRouteProvider.cs
+++ Modules/Web/REST/CustomDirectRouteProvider.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Web.Http.Controllers;
 using System.Web.Http.Routing;
 
@@ -6,9 +7,57 @@ namespace Nyan.Modules.Web.REST
 {
     public class CustomDirectRouteProvider : DefaultDirectRouteProvider
     {
+        public class RouteInfo
+        {
+            public string Class;
+            public string Route;
+            public string Method;
+
+            public override string ToString()
+            {
+                return Method.PadLeft(6) + " : " + Route;
+            }
+        }
+
+        public static readonly List<RouteInfo> Routes = new List<RouteInfo>();
+
         protected override IReadOnlyList<IDirectRouteFactory> GetActionRouteFactories(HttpActionDescriptor actionDescriptor)
         {
-            return actionDescriptor.GetCustomAttributes<IDirectRouteFactory>(true);
+            var ret = actionDescriptor.GetCustomAttributes<IDirectRouteFactory>(true);
+
+            List<System.Web.Http.RouteAttribute> a = new List<System.Web.Http.RouteAttribute>();
+
+            foreach (var item in ret)
+            {
+                string routeprefix = null;
+
+                if (item.GetType() == typeof(System.Web.Http.RouteAttribute))
+                {
+                    var subRoute = ((System.Web.Http.RouteAttribute)item).Template;
+
+                    var method = actionDescriptor.SupportedHttpMethods[0].Method;
+
+                    var attribs = actionDescriptor.ControllerDescriptor.ControllerType.CustomAttributes.ToList();
+
+                    foreach (var attrib in attribs)
+                    {
+                        if (attrib.AttributeType == typeof(System.Web.Http.RoutePrefixAttribute))
+                        {
+                            routeprefix = attrib.ConstructorArguments[0].Value.ToString();
+                        }
+
+                    }
+
+                    Routes.Add(new RouteInfo()
+                    {
+                        Class = actionDescriptor.ControllerDescriptor.ControllerType.FullName,
+                        Route = routeprefix + "/" + subRoute,
+                        Method = method
+                    });
+                }
+            }
+
+            return ret;
         }
     }
 }
