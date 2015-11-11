f88dd58594a7fdfd50652c78ff2747ff84391490
lbotinelly <lmbs001@bucknell.edu>
2015-11-11 17:01:10 -0500

    Code cleanup

--- Modules/Web/REST/CustomDirectRouteProvider.cs
+++ Modules/Web/REST/CustomDirectRouteProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
 using System.Web.Http.Controllers;
 using System.Web.Http.Routing;
 
@@ -7,57 +8,53 @@ namespace Nyan.Modules.Web.REST
 {
     public class CustomDirectRouteProvider : DefaultDirectRouteProvider
     {
+        public static readonly List<RouteInfo> Routes = new List<RouteInfo>();
+
+        protected override IReadOnlyList<IDirectRouteFactory> GetActionRouteFactories(
+            HttpActionDescriptor actionDescriptor)
+        {
+            var a = new List<RouteAttribute>();
+
+            var ret = actionDescriptor.GetCustomAttributes<IDirectRouteFactory>(true);
+            
+            foreach (var item in ret)
+            {
+                string routeprefix = null;
+
+                var attribute = item as RouteAttribute;
+
+                if (attribute == null) continue;
+
+                var subRoute = attribute.Template;
+
+                var method = actionDescriptor.SupportedHttpMethods[0].Method;
+
+                var attribs = actionDescriptor.ControllerDescriptor.ControllerType.CustomAttributes.ToList();
+
+                foreach (var attrib in attribs.Where(attrib => attrib.AttributeType == typeof(RoutePrefixAttribute)))
+                    routeprefix = attrib.ConstructorArguments[0].Value.ToString();
+
+                Routes.Add(new RouteInfo
+                {
+                    Class = actionDescriptor.ControllerDescriptor.ControllerType.FullName,
+                    Route = routeprefix + "/" + subRoute,
+                    Method = method
+                });
+            }
+
+            return ret;
+        }
+
         public class RouteInfo
         {
             public string Class;
-            public string Route;
             public string Method;
+            public string Route;
 
             public override string ToString()
             {
                 return Method.PadLeft(6) + " : " + Route;
             }
-        }
-
-        public static readonly List<RouteInfo> Routes = new List<RouteInfo>();
-
-        protected override IReadOnlyList<IDirectRouteFactory> GetActionRouteFactories(HttpActionDescriptor actionDescriptor)
-        {
-            var ret = actionDescriptor.GetCustomAttributes<IDirectRouteFactory>(true);
-
-            List<System.Web.Http.RouteAttribute> a = new List<System.Web.Http.RouteAttribute>();
-
-            foreach (var item in ret)
-            {
-                string routeprefix = null;
-
-                if (item.GetType() == typeof(System.Web.Http.RouteAttribute))
-                {
-                    var subRoute = ((System.Web.Http.RouteAttribute)item).Template;
-
-                    var method = actionDescriptor.SupportedHttpMethods[0].Method;
-
-                    var attribs = actionDescriptor.ControllerDescriptor.ControllerType.CustomAttributes.ToList();
-
-                    foreach (var attrib in attribs)
-                    {
-                        if (attrib.AttributeType == typeof(System.Web.Http.RoutePrefixAttribute))
-                        {
-                            routeprefix = attrib.ConstructorArguments[0].Value.ToString();
-                        }
-
-                    }
-
-                    Routes.Add(new RouteInfo()
-                    {
-                        Class = actionDescriptor.ControllerDescriptor.ControllerType.FullName,
-                        Route = routeprefix + "/" + subRoute,
-                        Method = method
-                    });
-                }
-            }
-
-            return ret;
         }
     }
 }
