70da89a7e80bff3ccd0a84d5ef32ddfdff849ba8
lbotinelly <lmbs001@bucknell.edu>
2015-11-12 14:21:27 -0500

    Added mini-GUID generator for better process tracking

---
+++ Core/Factories/Identifier.cs
@@ -0,0 +1,28 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Nyan.Core.Factories
+{
+    public static class Identifier
+    {
+        private const string A = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+        private const int MaxSize = 8;
+
+        public static string ShortGuid()
+        {
+            var chars = A.ToCharArray();
+            var data = new byte[1];
+            var crypto = new RNGCryptoServiceProvider();
+            crypto.GetNonZeroBytes(data);
+            const int size = MaxSize;
+            data = new byte[size];
+            crypto.GetNonZeroBytes(data);
+            var result = new StringBuilder(size);
+            
+            foreach (var b in data)
+                result.Append(chars[b%(chars.Length - 1)]);
+
+            return result.ToString();
+        }
+    }
+}
