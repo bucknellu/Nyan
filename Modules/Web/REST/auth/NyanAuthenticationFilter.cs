8cadac7a50589cdad51ae816c575e7772c4f31f6
lbotinelly <lmbs001@bucknell.edu>
2015-11-18 14:42:24 -0500

    Optimization. Removed log entries.

--- Modules/Web/REST/auth/NyanAuthenticationFilter.cs
+++ Modules/Web/REST/auth/NyanAuthenticationFilter.cs
@@ -2,9 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web.Http.Filters;
-using Nyan.Core.Extensions;
 using Nyan.Core.Modules.Identity;
-using Nyan.Core.Modules.Log;
 using Nyan.Core.Settings;
 
 namespace Nyan.Modules.Web.REST.auth
@@ -14,12 +12,12 @@ namespace Nyan.Modules.Web.REST.auth
 
     public class NyanAuthenticationFilter : IAuthenticationFilter
     {
+        private static readonly NyanPrincipal NyanPrincipalInstance = new NyanPrincipal();
+
         private static IRESTAuthenticationFilter CurrentFilter
         {
             get { return Current.Authorization as IRESTAuthenticationFilter; }
         }
-
-        private static readonly NyanPrincipal NyanPrincipalInstance = new NyanPrincipal();
 
         public IPrincipal Principal
         {
@@ -33,37 +31,24 @@ namespace Nyan.Modules.Web.REST.auth
 
         public Task AuthenticateAsync(HttpAuthenticationContext context, CancellationToken cancellationToken)
         {
-
             if (CurrentFilter == null) return Task.FromResult(0);
 
             context.Principal = Principal;
-
-            Current.Log.Add(CurrentFilter.GetType().Name + ": AuthenticateAsync", Message.EContentType.MoreInfo);
             return CurrentFilter.AuthenticateAsync(context, cancellationToken);
         }
 
         public Task ChallengeAsync(HttpAuthenticationChallengeContext context, CancellationToken cancellationToken)
         {
-            if (CurrentFilter == null) return Task.FromResult(0);
-
-            Current.Log.Add(CurrentFilter.GetType().Name + ": ChallengeAsync", Message.EContentType.MoreInfo);
-            return CurrentFilter.ChallengeAsync(context, cancellationToken);
+            return CurrentFilter == null ? Task.FromResult(0) : CurrentFilter.ChallengeAsync(context, cancellationToken);
         }
 
         public bool AllowMultiple
         {
-            get
-            {
-                if (CurrentFilter == null) return false;
-
-                Current.Log.Add(CurrentFilter.GetType().Name + ": AllowMultiple", Message.EContentType.MoreInfo);
-                return CurrentFilter.AllowMultiple;
-            }
+            get { return CurrentFilter != null && CurrentFilter.AllowMultiple; }
         }
 
         public virtual bool CheckPermission(string pCode)
         {
-            Current.Log.Add(Current.Authorization.GetType().Name + ": CheckPermission [{0}]".format(pCode), Message.EContentType.MoreInfo);
             return Current.Authorization.CheckPermission(pCode);
         }
 
@@ -76,10 +61,7 @@ namespace Nyan.Modules.Web.REST.auth
 
             public IIdentity Identity
             {
-                get
-                {
-                    return CurrentFilter == null ? null : CurrentFilter.Identity;
-                }
+                get { return CurrentFilter == null ? null : CurrentFilter.Identity; }
             }
         }
     }
