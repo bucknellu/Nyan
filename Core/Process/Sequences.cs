643b2839df086c7ed6de60f9856be16a165edd18
lbotinelly <lmbs001@bucknell.edu>
2015-11-23 13:39:17 -0500

    Moved Shutdown sequence to main module

---
+++ Core/Process/Sequences.cs
@@ -0,0 +1,32 @@
+﻿using Nyan.Core.Modules.Log;
+using Nyan.Core.Settings;
+
+namespace Nyan.Core.Process
+{
+    public static class Sequences
+    {
+        public static void End(string pReason = "(None)")
+        {
+            Current.Log.Add("Stack shutdown initiated: " + pReason, Message.EContentType.ShutdownSequence);
+
+            Current.Log.Add("Shutting down " + Current.Authorization.GetType().Name, Message.EContentType.MoreInfo);
+            Current.Authorization.Shutdown();
+
+            Current.Log.Add("Shutting down " + Current.Cache.GetType().Name, Message.EContentType.MoreInfo);
+            Current.Cache.Shutdown();
+
+            Current.Log.Add("Shutting down " + Current.Scope.GetType().Name, Message.EContentType.MoreInfo);
+            Current.Scope.Shutdown();
+
+            Current.Log.Add("Shutting down " + Current.Encryption.GetType().Name, Message.EContentType.MoreInfo);
+            Current.Encryption.Shutdown();
+
+            Current.Log.Add(@"  _|\_/|  ZZZzzz", Message.EContentType.Info);
+            Current.Log.Add(@"c(_(-.-)", Message.EContentType.Info);
+
+            Current.Log.Add(@"Stack shutdown concluded.", Message.EContentType.ShutdownSequence);
+
+            Current.Log.Shutdown();
+        }
+    }
+}
--- Modules/Web/REST/Sequences.cs
+++ Modules/Web/REST/Sequences.cs
@@ -35,35 +35,13 @@ namespace Nyan.Modules.Web.REST
                     BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField,
                     null, runtime, null);
 
-                Current.Log.Add("Stack shutdown initiated: " + shutDownMessage.Replace(Environment.NewLine, ":").Split(':')[0], Message.EContentType.ShutdownSequence);
+                var msg = shutDownMessage.Replace(Environment.NewLine, ":").Split(':')[0];
+
+                Core.Process.Sequences.End(msg);
+
             }
             else
-                Current.Log.Add("Stack shutdown started", Message.EContentType.ShutdownSequence);
-
-            if (Current.Log.UseScheduler)
-            {
-                Current.Log.UseScheduler = false;
-                Current.Log.Add("Log Scheduler switched off", Message.EContentType.MoreInfo);
-            }
-
-            Current.Log.Add("Shutting down " + Current.Authorization.GetType().Name, Message.EContentType.MoreInfo);
-            Current.Authorization.Shutdown();
-
-            Current.Log.Add("Shutting down " + Current.Cache.GetType().Name, Message.EContentType.MoreInfo);
-            Current.Cache.Shutdown();
-
-            Current.Log.Add("Shutting down " + Current.Scope.GetType().Name, Message.EContentType.MoreInfo);
-            Current.Scope.Shutdown();
-
-            Current.Log.Add("Shutting down " + Current.Encryption.GetType().Name, Message.EContentType.MoreInfo);
-            Current.Encryption.Shutdown();
-
-            Current.Log.Add(@"  _|\_/|  ZZZzzz", Message.EContentType.Info);
-            Current.Log.Add(@"c(_(-.-)", Message.EContentType.Info);
-
-            Current.Log.Add(@"Stack shutdown concluded.", Message.EContentType.ShutdownSequence);
-
-            Current.Log.Shutdown();
+                Core.Process.Sequences.End();
         }
     }
 }
