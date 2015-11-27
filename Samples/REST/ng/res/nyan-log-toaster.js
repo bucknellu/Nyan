3c6b908e7cf8c199d4cc78de4fc6796c2a920fd1
Leo Botinelly <lmbs001@bucknell.edu>
2015-11-27 01:26:10 -0500

    REST Sample: nyan-log-toaster, original implementation

---
+++ Samples/REST/ng/res/nyan-log-toaster.js
@@ -0,0 +1,55 @@
+﻿(function (window, angular) {
+
+    'use strict';
+
+    var nyanStackModule = angular.module('ngNyanLogToaster', ['ngNyanStack', 'toaster']);
+
+    nyanStackModule
+    .service(
+    "nyanLogToasterService",
+    function (nyanLogPipelineService, toaster) {
+
+
+        this.log = function () {
+            //Sit on this one.
+        }
+
+        this.info = function () {
+            toaster.pop({
+                type: 'success',
+                title: arguments[0],
+                body: arguments[1]
+            });
+        }
+
+        this.warn = function () {
+            toaster.pop({
+                type: 'warning',
+                title: arguments[0],
+                body: arguments[1]
+            });
+        }
+
+        this.error = function () {
+            toaster.pop({
+                type: 'error',
+                title: arguments[0],
+                body: arguments[1]
+            });
+        }
+
+        this.debug = function () {
+            toaster.pop({
+                type: 'info',
+                title: arguments[0],
+                body: arguments[1]
+            });
+        }
+
+        nyanLogPipelineService.register(this);
+
+    }).run(function (nyanLogToasterService) {
+        console.log('nyanLogToasterService: Start');
+    })
+}
+)(window, window.angular);
