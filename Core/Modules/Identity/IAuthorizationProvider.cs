bd93e3c39259d0518fe5b78b9cb67f33d71d0155
lbotinelly <lmbs001@bucknell.edu>
2016-01-21 15:20:14 -0500

    Added generic ID and Locator properties to IAuthorizationProvider

--- Core/Modules/Identity/IAuthorizationProvider.cs
+++ Core/Modules/Identity/IAuthorizationProvider.cs
@@ -5,6 +5,8 @@ namespace Nyan.Core.Modules.Identity
     public interface IAuthorizationProvider
     {
         IIdentity Identity { get; }
+        string Id { get; }
+        string Locator { get; }
         bool CheckPermission(string pCode);
         void Shutdown();
     }
