49c8d9d889e884c0aa943db31932206ad87ed6d1
lbotinelly <lmbs001@bucknell.edu>
2016-02-05 10:04:40 -0500

    NamedPipes log implementation

---
+++ Modules/Log/NamedPipes/NamedPipesLogProvider.cs
@@ -0,0 +1,102 @@
+﻿using NamedPipeWrapper;
+using Nyan.Core.Extensions;
+using Nyan.Core.Modules.Log;
+using System;
+using System.Diagnostics;
+using System.IO.Pipes;
+
+namespace Nyan.Modules.Log.NamedPipes
+{
+
+    public class NamedPipesLogProvider : LogProvider
+    {
+        private NamedPipeServer<string> _in;
+        private NamedPipeClient<string> _out;
+
+        private PipeSecurity _ps;
+
+        private string _targetAddress = "";
+
+        public NamedPipesLogProvider()
+        {
+            Initialize("NYAN_LOG_PIPE");
+        }
+
+        public NamedPipesLogProvider(string targetAddress) { Initialize(targetAddress); }
+
+        public override string Protocol { get { return null; } }
+
+        public override string Uri { get { return _targetAddress; } }
+
+        public override void Shutdown()
+        {
+            //if (_in != null) _in.Terminate();
+            //if (_out != null) _out.Terminate();
+        }
+
+        public override event Message.MessageArrivedHandler MessageArrived;
+
+        private void Initialize(string targetAddress)
+        {
+            try
+            {
+
+                _targetAddress = targetAddress;
+
+                StartListening(); //Required - pipe needs to exist
+
+                _out = new NamedPipeClient<string>(_targetAddress);
+                _out.Start();
+
+                UseScheduler = true;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public override void Dispatch(Message payload)
+        {
+            if (Environment.UserInteractive)
+            {
+                Console.WriteLine(payload.Content);
+                Debug.WriteLine(payload.Content);
+            }
+
+            try { beforeSend(payload); } catch (Exception e) { Core.Settings.Current.Log.Add(e); }
+
+            var transformedPayload = payload.ToJson().Encrypt();
+
+            _out.PushMessage(transformedPayload);
+
+            //there is more than 1 server present
+        }
+
+        public virtual void beforeSend(Message payload) { }
+
+        public override void StartListening()
+        {
+
+            if (_in != null) return;
+
+            _in = new NamedPipeServer<string>(_targetAddress);
+            _in.ClientMessage += _in_ClientMessage;
+            _in.Start();
+        }
+
+        private void _in_ClientMessage(NamedPipeConnection<string, string> connection, string message)
+        {
+            var _out = message.Decrypt().FromJson<Message>();
+            if (_out == null) return;
+            MessageArrived(_out);
+        }
+
+        private void _gateway_MessageArrived(Message message)
+        {
+            if (MessageArrived == null) return;
+            MessageArrived(message);
+        }
+    }
+}
+
---
+++ Modules/Log/NamedPipes/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyDescription("A named pipes Log module for the Nyan data service stack")]
+[assembly: AssemblyCompany("Nyan Development Team")]
+[assembly: AssemblyCopyright("Copyright © 2015 Nyan Development Team")]
+[assembly: AssemblyTitle("Nyan.Modules.Log.NamedPipes")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyProduct("Nyan.Modules.Log.NamedPipes")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("5dff0b79-e5dc-47b8-8793-48781a0be8da")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
