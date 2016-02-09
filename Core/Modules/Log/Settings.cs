3da741a5826c9b56c41cf4dcd0b8ed06ff17770f
lbotinelly <lmbs001@bucknell.edu>
2016-02-08 19:32:17 -0500

    Verbosity Threshold now part of local Log settings

--- Core/Modules/Log/Settings.cs
+++ Core/Modules/Log/Settings.cs
@@ -3,5 +3,6 @@
     public static class Settings
     {
         public static bool replicateLocally = false;
+        public static Message.EContentType VerbosityThreshold = Message.EContentType.Debug;
     }
 }
