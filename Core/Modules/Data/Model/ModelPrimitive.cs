886904b96cd91d98490280bf3ee491681b11a48e
lbotinelly <lmbs001@bucknell.edu>
2016-02-08 19:30:25 -0500

    First stab at Model separation

---
+++ Core/Modules/Data/Model/ModelPrimitive.cs
@@ -0,0 +1,41 @@
+﻿using Nyan.Core.Modules.Data.Adapter;
+
+namespace Nyan.Core.Modules.Data.Model
+{
+    public abstract class ModelPrimitive<T> where T : ModelPrimitive<T>
+    {
+        #region Control objects
+        private static readonly object AccessLock = new object();
+
+        private T localInstance = null;
+
+        #endregion
+
+        #region Static methods
+
+        #endregion       
+
+        #region Instantiated methods used by Static interface members
+
+        #endregion
+
+        #region Instance methods
+
+        public abstract string GetEntityIdentifier(ModelPrimitive<T> oRef = null);
+        public abstract void Insert();
+        public abstract bool IsNew();
+        public abstract bool IsReadOnly();
+        public abstract void OnInsert();
+        public abstract void OnRemove();
+        public abstract void OnSave(string newIdentifier);
+        public abstract void Remove();
+        public abstract string Save();
+        public abstract string SaveAndGetId();
+        public abstract string SaveAndGetId(DynamicParametersPrimitive obj);
+        public abstract void SetEntityIdentifier(object value);
+
+        #endregion
+
+        public ModelPrimitive() { }
+    }
+}
---
+++ Core/Modules/Data/Model/ObjectModel.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Nyan.Core.Modules.Data.Adapter;
+
+namespace Nyan.Core.Modules.Data.Model
+{
+    public class ObjectModel : ModelPrimitive<ObjectModel>
+    {
+        public override string GetEntityIdentifier(ModelPrimitive<ObjectModel> oRef = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Insert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsNew()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override bool IsReadOnly()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnInsert()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnRemove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OnSave(string newIdentifier)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Remove()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SaveAndGetId()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string SaveAndGetId(DynamicParametersPrimitive obj)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void SetEntityIdentifier(object value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
