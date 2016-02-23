a62d3cdf69e0c7e9b51878bef9e75a517268c6f7
lbotinelly <lmbs001@bucknell.edu>
2016-02-23 16:23:00 -0500

    Cache for simple Credential resolution

--- Core/Modules/Data/Connection/Factory.cs
+++ Core/Modules/Data/Connection/Factory.cs
@@ -10,47 +10,65 @@ namespace Nyan.Core.Modules.Data.Connection
 {
     public static class Factory
     {
+
+        private static readonly Dictionary<ConnectionBundlePrimitive, CredentialSetPrimitive> Cache = new Dictionary<ConnectionBundlePrimitive, CredentialSetPrimitive>();
+        private static readonly object OLock = new object();
+
+
         public static CredentialSetPrimitive GetCredentialSetPerConnectionBundle(ConnectionBundlePrimitive pConn, Type pPrefCredSetType = null)
         {
-            var ret = new CredentialSetPrimitive
+            lock (OLock)
             {
-                CredentialCypherKeys = new Dictionary<string, string>(),
-                AssociatedBundleType = pConn.GetType()
-            };
-
-            var probeTypes = new List<Type>();
-
-            if (pPrefCredSetType != null)
-                probeTypes.Add(pPrefCredSetType);
-
-            var scanModules = Management.GetClassesByInterface<CredentialSetPrimitive>();
-
-            probeTypes = probeTypes.Concat(scanModules).ToList();
-
-            // Create instances for all probed Credential types;
-            var creds = probeTypes.Select(i => i.CreateInstance<CredentialSetPrimitive>()).ToList();
-
-            // Filter Instances out, based on target Connection Bundle:
-
-            var tmpCreds = creds.Where(i => i.AssociatedBundleType.ToString() == ret.AssociatedBundleType.ToString()).ToList();
-
-            creds = tmpCreds;
-
-            // now, compile all entries, using definition order;
-
-            if (creds.Count > 0)
-                Current.Log.Add("[" + ret.AssociatedBundleType + "] Credential sets: " + string.Join(",", creds.Select(i=> "[" + i.GetType().Name + "]")), Message.EContentType.Info);
-
-            foreach (var i in creds)
-            {
-                foreach (var ii in i.CredentialCypherKeys)
+                if (pPrefCredSetType == null)
                 {
-                    if (!ret.CredentialCypherKeys.ContainsKey(ii.Key))
-                        ret.CredentialCypherKeys[ii.Key] = ii.Value;
+                    if (Cache.ContainsKey(pConn)) return Cache[pConn];
                 }
-            }
 
-            return ret;
+                var ret = new CredentialSetPrimitive
+                {
+                    CredentialCypherKeys = new Dictionary<string, string>(),
+                    AssociatedBundleType = pConn.GetType()
+                };
+
+                var probeTypes = new List<Type>();
+
+                if (pPrefCredSetType != null)
+                {
+                    probeTypes.Add(pPrefCredSetType);
+                }
+
+                var scanModules = Management.GetClassesByInterface<CredentialSetPrimitive>();
+
+                probeTypes = probeTypes.Concat(scanModules).ToList();
+
+                // Create instances for all probed Credential types;
+                var creds = probeTypes.Select(i => i.CreateInstance<CredentialSetPrimitive>()).ToList();
+
+                // Filter Instances out, based on target Connection Bundle:
+
+                var tmpCreds = creds.Where(i => i.AssociatedBundleType.ToString() == ret.AssociatedBundleType.ToString()).ToList();
+
+                creds = tmpCreds;
+
+                // now, compile all entries, using definition order;
+
+                if (creds.Count > 0)
+                    Current.Log.Add("[" + ret.AssociatedBundleType + "] Credential sets: " + string.Join(",", creds.Select(i => "[" + i.GetType().Name + "]")), Message.EContentType.Info);
+
+                foreach (var i in creds)
+                {
+                    foreach (var ii in i.CredentialCypherKeys)
+                    {
+                        if (!ret.CredentialCypherKeys.ContainsKey(ii.Key))
+                            ret.CredentialCypherKeys[ii.Key] = ii.Value;
+                    }
+                }
+
+                if (pPrefCredSetType == null)
+                    Cache[pConn] = ret;
+
+                return ret;
+            }
         }
     }
 }
