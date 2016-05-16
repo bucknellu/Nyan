1173a2b2a45b7a4675bc3a350d07ee6eb0d8f9b3
lbotinelly <lmbs001@bucknell.edu>
2016-05-16 10:55:58 -0400

    Added ReferenceField to GET method

--- Core/Modules/Data/ComplexMicroEntity.cs
+++ Core/Modules/Data/ComplexMicroEntity.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using Nyan.Core.Extensions;
+using Nyan.Core.Modules.Cache;
 using Nyan.Core.Settings;
 
 namespace Nyan.Core.Modules.Data
@@ -10,13 +12,25 @@ namespace Nyan.Core.Modules.Data
         where T : ComplexMicroEntity<T, TU>, new()
         where TU : MicroEntity<TU>
     {
-        public static T Get(long identifier)
+        public static T Get(long identifier, string referenceField = null)
         {
-            return Get(identifier.ToString(CultureInfo.InvariantCulture));
+            return Get(identifier.ToString(CultureInfo.InvariantCulture), referenceField);
         }
 
         public static T Get(string identifier, string referenceField = null)
         {
+            var cacheid = typeof(T).CacheKey(identifier);
+
+
+            if (Current.Cache.OperationalStatus == EOperationalStatus.Operational)
+            {
+
+                var cache = Current.Cache[cacheid].FromJson<T>();
+                if (cache != null)
+                { return cache; }
+            }
+
+
             var ret = new T();
             TU probe;
 
@@ -33,10 +47,13 @@ namespace Nyan.Core.Modules.Data
 
             var ret2 = new T();
 
-            try { ret2 = ret.CastToComplexType((probe)); } catch (Exception ee)
+            try { ret2 = ret.CastToComplexType(probe); }
+            catch (Exception ee)
             {
                 Current.Log.Add(ee);
             }
+
+            Current.Cache[cacheid] = ret2.ToJson();
 
             return ret2;
         }
