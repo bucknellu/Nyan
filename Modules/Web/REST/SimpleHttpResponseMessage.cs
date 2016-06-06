62c499698a7fcb02f24be569d35d0c4bb715af47
lbotinelly <lmbs001@bucknell.edu>
2016-06-06 15:27:29 -0400

    Stub - wrapper for common HttpResponseMessages

---
+++ Modules/Web/REST/SimpleHttpResponseMessage.cs
@@ -0,0 +1,16 @@
+﻿using System.Net;
+using System.Net.Http;
+
+namespace Nyan.Modules.Web.REST
+{
+    public class SimpleHttpResponseMessage: HttpResponseMessage
+    {
+
+        public SimpleHttpResponseMessage(HttpStatusCode code, string content)
+        {
+            StatusCode = code;
+            Content = new StringContent(content);
+        }
+
+    }
+}
