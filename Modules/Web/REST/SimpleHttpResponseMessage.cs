03ca177487a1581c557b50dd8cb80a567b67ef06
lbotinelly <lmbs001@bucknell.edu>
2016-07-13 08:29:22 -0400

    Simple Http Response wrapper

--- Modules/Web/REST/SimpleHttpResponseMessage.cs
+++ Modules/Web/REST/SimpleHttpResponseMessage.cs
@@ -1,10 +1,18 @@
-﻿using System.Net;
+﻿using System;
+using System.Net;
 using System.Net.Http;
+using Nyan.Core.Settings;
 
 namespace Nyan.Modules.Web.REST
 {
     public class SimpleHttpResponseMessage: HttpResponseMessage
     {
+        public SimpleHttpResponseMessage(Exception e)
+        {
+            StatusCode = HttpStatusCode.InternalServerError;
+            Content = new StringContent("Error: " + e.Message);
+            Current.Log.Add(e);
+        }
 
         public SimpleHttpResponseMessage(HttpStatusCode code, string content)
         {
