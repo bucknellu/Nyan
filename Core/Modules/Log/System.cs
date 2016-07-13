37cb6020ed8500c37e34bffd7219f05e4e5c7410
lbotinelly <lmbs001@bucknell.edu>
2016-07-13 08:30:35 -0400

    Cleanup

--- Core/Modules/Log/System.cs
+++ Core/Modules/Log/System.cs
@@ -1,18 +1,13 @@
-﻿using Nyan.Core.Diagnostics;
-using Nyan.Core.Extensions;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Nyan.Core.Extensions;
 
 namespace Nyan.Core.Modules.Log
 {
     public static class System
     {
-        static private StreamWriter _out;
+        private static readonly StreamWriter _out;
 
         static System()
         {
@@ -24,18 +19,16 @@ namespace Nyan.Core.Modules.Log
             _out = new StreamWriter(new FileStream(dir + "\\log.txt", FileMode.Append, FileAccess.Write, FileShare.ReadWrite));
         }
 
-        public static void Add(Exception e)
-        {
-            Add("Exception: " + e.Message + " at " + new StackTrace(e, true).FancyString(), Message.EContentType.Exception);
-        }
-        public static void Add(string message, Log.Message.EContentType type = Message.EContentType.Info)
+        public static void Add(Exception e) { Add("Exception: " + e.Message + " at " + new StackTrace(e, true).FancyString(), Message.EContentType.Exception); }
+
+        public static void Add(string message, Message.EContentType type = Message.EContentType.Info)
         {
             var dir = Configuration.DataDirectory + "\\log";
 
             if (!Directory.Exists(dir))
                 Directory.CreateDirectory(dir);
 
-            var ctn = DateTime.Now.ToString() + " " + ("[" + type.ToString() + "]").PadRight(20) + " " + message;
+            var ctn = DateTime.Now + " " + ("[" + type + "]").PadRight(20) + " " + message;
 
             Console.WriteLine(ctn);
             Debug.Print(ctn);
