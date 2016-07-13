e587a66aa63ad4a49d45db38c95e5ea199632d88
lbotinelly <lmbs001@bucknell.edu>
2016-07-13 08:28:08 -0400

    GlobalAssemblyResolver

---
+++ Modules/Web/REST/GlobalAssemblyResolver.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Web.Http.Dispatcher;
+using Nyan.Core.Assembly;
+using Nyan.Core.Extensions;
+
+namespace Nyan.Modules.Web.REST
+{
+    public class GlobalAssemblyResolver : IAssembliesResolver
+    {
+        public ICollection<Assembly> GetAssemblies()
+        {
+            // Loads all assemblies mapped during resolution time:
+            var baseAssemblies = Management.AssemblyCache.Select(i => i.Value).ToList().ToCollection();
+            return baseAssemblies;
+        }
+    }
+}
