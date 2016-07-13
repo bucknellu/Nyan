1ac0bff97937452f83ecfcc3323945a3228d5f51
lbotinelly <lmbs001@bucknell.edu>
2016-07-13 08:27:41 -0400

    Removed cache control from controller

--- Modules/Web/REST/ComplexEntityWebApiController.cs
+++ Modules/Web/REST/ComplexEntityWebApiController.cs
@@ -2,7 +2,6 @@
 using System.Diagnostics;
 using System.Linq;
 using System.Net;
-using System.Runtime.Caching;
 using System.Web.Http;
 using Nyan.Core.Modules.Data;
 using Nyan.Core.Settings;
@@ -16,9 +15,6 @@ namespace Nyan.Modules.Web.REST
     {
         // ReSharper disable once InconsistentNaming
 
-        // ReSharper disable once StaticFieldInGenericType
-        private static readonly ObjectCache cache = new MemoryCache("ComplexEntityWebApiController");
-
         [Route("")]
         [HttpGet]
         public virtual object WebApiGetAll()
@@ -26,28 +22,19 @@ namespace Nyan.Modules.Web.REST
             var sw = new Stopwatch();
             sw.Start();
 
-            var cacheKey = typeof (T).FullName + ":list";
-
-            if (!cache.Contains(cacheKey))
+            try
             {
-                try
-                {
-                    var ret = ComplexMicroEntity<T, TU>.ConvertFromBaseList(MicroEntity<TU>.Get().ToList());
-                    cache.Set(cacheKey, ret, new CacheItemPolicy {AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(10)});
-
-                    sw.Stop();
-                    Current.Log.Add("  GET " + typeof (T).FullName + " OK (" + sw.ElapsedMilliseconds + " ms)");
-                    return ret;
-                } catch (Exception e)
-                {
-                    sw.Stop();
-                    Current.Log.Add("  GET " + typeof (T).FullName + " ERR (" + sw.ElapsedMilliseconds + " ms): " + e.Message, e);
-                    throw;
-                }
+                var ret = ComplexMicroEntity<T, TU>.Get().ToList();
+                sw.Stop();
+                Current.Log.Add("  GET " + typeof(T).FullName + " OK (" + sw.ElapsedMilliseconds + " ms)");
+                return ret;
             }
-
-            Current.Log.Add("  GET " + typeof (T).FullName + " CACHEHIT (" + sw.ElapsedMilliseconds + " ms)");
-            return cache[cacheKey];
+            catch (Exception e)
+            {
+                sw.Stop();
+                Current.Log.Add("  GET " + typeof(T).FullName + " ERR (" + sw.ElapsedMilliseconds + " ms): " + e.Message, e);
+                throw;
+            }
         }
 
         [Route("new")]
@@ -59,14 +46,15 @@ namespace Nyan.Modules.Web.REST
 
             try
             {
-                var ret = (T) Activator.CreateInstance(typeof (T), new object[] {});
+                var ret = (T)Activator.CreateInstance(typeof(T), new object[] { });
                 sw.Stop();
-                Current.Log.Add("  NEW " + typeof (T).FullName + " OK (" + sw.ElapsedMilliseconds + " ms)");
+                Current.Log.Add("  NEW " + typeof(T).FullName + " OK (" + sw.ElapsedMilliseconds + " ms)");
                 return ret;
-            } catch (Exception e)
+            }
+            catch (Exception e)
             {
                 sw.Stop();
-                Current.Log.Add("  NEW " + typeof (T).FullName + " ERR (" + sw.ElapsedMilliseconds + " ms): " + e.Message, e);
+                Current.Log.Add("  NEW " + typeof(T).FullName + " ERR (" + sw.ElapsedMilliseconds + " ms): " + e.Message, e);
                 throw;
             }
         }
@@ -78,31 +66,20 @@ namespace Nyan.Modules.Web.REST
             var sw = new Stopwatch();
             sw.Start();
 
-            var cacheKey = typeof (T).FullName + ":" + id;
-
-            if (!cache.Contains(cacheKey))
+            try
             {
-                try
-                {
-                    var ret = ComplexMicroEntity<T, TU>.Get(MicroEntity<TU>.Get(id));
-                    if (ret == null) throw new HttpResponseException(HttpStatusCode.NotFound);
+                var ret = ComplexMicroEntity<T, TU>.Get(MicroEntity<TU>.Get(id));
+                if (ret == null) throw new HttpResponseException(HttpStatusCode.NotFound);
 
-                    cache.Set(cacheKey, ret, new CacheItemPolicy {AbsoluteExpiration = DateTimeOffset.UtcNow.AddMinutes(10)});
-
-                    Current.Log.Add("  GET " + typeof (T).FullName + ":" + id + " OK (" + sw.ElapsedMilliseconds + " ms)");
-                    return ret;
-                } catch (Exception e)
-                {
-                    sw.Stop();
-                    Current.Log.Add(
-                        "  GET:" + id + " " + typeof (T).FullName + ":" + id + " ERR (" + sw.ElapsedMilliseconds + " ms): " +
-                        e.Message, e);
-                    throw;
-                }
+                Current.Log.Add("  GET " + typeof(T).FullName + ":" + id + " OK (" + sw.ElapsedMilliseconds + " ms)");
+                return ret;
             }
-
-            Current.Log.Add("  GET " + typeof (T).FullName + ":" + id + " CACHEHIT (" + sw.ElapsedMilliseconds + " ms)");
-            return cache[cacheKey];
+            catch (Exception e)
+            {
+                sw.Stop();
+                Current.Log.Add("  GET:" + id + " " + typeof(T).FullName + ":" + id + " ERR (" + sw.ElapsedMilliseconds + " ms): " + e.Message, e);
+                throw;
+            }
         }
     }
 }
