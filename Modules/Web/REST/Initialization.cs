7d69aa42321d795144f7a238a31a0b17c97c8361
Leo Botinelly <leo.botinelly@bucknell.edu>
2016-10-04 15:33:38 -0400

    CSV output format support

--- Modules/Web/REST/Initialization.cs
+++ Modules/Web/REST/Initialization.cs
@@ -1,9 +1,11 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using System.Web.Http.Cors;
 using Nyan.Core.Modules.Log;
 using Nyan.Core.Settings;
 using Nyan.Modules.Web.REST.auth;
+using Nyan.Modules.Web.REST.formatters;
 
 namespace Nyan.Modules.Web.REST
 {
@@ -18,8 +20,7 @@ namespace Nyan.Modules.Web.REST
             // Force load of all Controllers:
             if (Current.WebApiCORSDomains != null)
             {
-                var corsAttr = new EnableCorsAttribute(Current.WebApiCORSDomains, "*", "*");
-                corsAttr.SupportsCredentials = true;
+                var corsAttr = new EnableCorsAttribute(Current.WebApiCORSDomains, "*", "*") {SupportsCredentials = true};
                 config.EnableCors(corsAttr);
             }
 
@@ -28,7 +29,15 @@ namespace Nyan.Modules.Web.REST
             config.Filters.Add(new NyanAuthenticationFilter());
 
             config.MapHttpAttributeRoutes(new CustomDirectRouteProvider());
+
+            config.Formatters.Clear();
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.Add(new XmlMediaTypeFormatter());
+            config.Formatters.Add(new CsvMediaTypeFormatter());
+
             config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            //config.Formatters.Add(new CsvMediaTypeFormatter());
+
 
             Current.Log.Add("WebApi REST       : Routes registered.", Message.EContentType.Info);
         }
