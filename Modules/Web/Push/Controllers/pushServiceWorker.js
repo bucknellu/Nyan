36469beb69b0c5b0e84caeb6ba02eaf79a7791db
Leo Botinelly <leo.botinelly@bucknell.edu>
2016-11-11 16:15:37 -0500

    Added default link parsing

--- Modules/Web/Push/Controllers/pushServiceWorker.js
+++ Modules/Web/Push/Controllers/pushServiceWorker.js
@@ -19,59 +19,63 @@
 
 // Version 0.1
 
-'use strict';
+"use strict";
 
-console.log('Started', self);
+console.log("Started", self);
 
-self.addEventListener('install', function (event) {
+self.addEventListener("install", function (event) {
     self.skipWaiting();
-    console.log('Installed', event);
+    console.log("Installed", event);
 });
 
-self.addEventListener('activate', function (event) {
-    console.log('Activated', event);
-});
+self.addEventListener("activate", function (event) { console.log("Activated", event); });
 
-self.addEventListener('push', function (event) {
-    console.log('Push message event', event);
-    console.log('Push message arguments', arguments);
+self.addEventListener("push", function (event) {
+    console.log("Push message event", event);
+    console.log("Push message arguments", arguments);
 
     var data = null;
-
     if (event.data) {
         data = event.data.json();
-        console.log('Push message data', data);
+        console.log("Push message data", data);
     }
 
     event.waitUntil(
-      self.registration.showNotification(data.title, data.options));
+        self.registration.showNotification(data.title, data.options));
 });
 
-self.addEventListener('notificationclick', function (event) {
-    console.log('Notification click: tag', event.notification.tag);
+self.addEventListener("notificationclick", function (event) {
+
     // Android doesn't close the notification when you click it
     // See http://crbug.com/463146
     event.notification.close();
-    var url = 'https://youtu.be/gYMkEMCHtJ4';
+
+    var url = null;
+
+    try { url = event.notification.data.url; } catch (e) { }
+
+    console.log("notificationclick: url", url);
+
     // Check if there's already a tab open with this URL.
     // If yes: focus on the tab.
     // If no: open a tab with the URL.
     event.waitUntil(
-      clients.matchAll({
-          type: 'window'
-      })
-      .then(function (windowClients) {
-          console.log('WindowClients', windowClients);
-          for (var i = 0; i < windowClients.length; i++) {
-              var client = windowClients[i];
-              console.log('WindowClient', client);
-              if (client.url === url && 'focus' in client) {
-                  return client.focus();
-              }
-          }
-          if (clients.openWindow) {
-              return clients.openWindow(url);
-          }
-      })
+        clients.matchAll({ type: "window" })
+        .then(function (windowClients) {
+            if (url) {
+                console.log("WindowClients", windowClients);
+                for (var i = 0; i < windowClients.length; i++) {
+                    var client = windowClients[i];
+                    console.log("WindowClient", client);
+                    if (client.url === url && "focus" in client) {
+                        return client.focus();
+                    }
+                }
+                if (clients.openWindow) {
+                    return clients.openWindow(url);
+                }
+
+            }
+        })
     );
 });
