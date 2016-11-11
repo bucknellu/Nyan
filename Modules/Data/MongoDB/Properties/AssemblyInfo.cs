1313ec49f5a7076bc38e4af35285476ca25549aa
Leo Botinelly <leo.botinelly@bucknell.edu>
2016-11-11 08:54:26 -0500

    Initial MongoDB client

---
+++ Modules/Data/MongoDB/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Nyan.Modules.Data.MongoDB")]
+[assembly: AssemblyDescription("A MongoDB database module for the Nyan data service stack")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nyan Development Team")]
+[assembly: AssemblyProduct("Nyan.Modules.Data.MongoDB")]
+[assembly: AssemblyCopyright("Copyright © 2016 Nyan Development Team")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4283a010-da68-431c-86d0-4db4a6613716")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.*")]
