fc35b60737d839e3330f510d83a8fa462fa04998
Leo Botinelly <leo.botinelly@bucknell.edu>
2016-11-11 16:15:54 -0500

    Added custom Data colection

--- Modules/Web/Push/Google/Notification/NotificationPrimitive.cs
+++ Modules/Web/Push/Google/Notification/NotificationPrimitive.cs
@@ -16,26 +16,23 @@ namespace Nyan.Modules.Web.Push.Google.Notification
         {
             public List<Action> actions = new List<Action>();
             public string body { get; set; }
-            public string targetUrl { get; set; }
             public string dir { get; set; } = "auto";
             public string lang { get; set; }
             public bool renotify { get; set; } = false;
             public bool requireInteraction { get; set; } = false;
             public string tag { get; set; }
             public List<int> vibrate { get; set; } = new List<int>();
-            public string data { get; set; }
+            public Dictionary<string, string> data { get; set; } = new Dictionary<string, string>();
             public string icon { get; set; }
             public string badge { get; set; }
             public class Action
             {
-                public Action(string action, string title = null, string icon = null, string url = null)
+                public Action(string action, string title = null, string icon = null)
                 {
                     this.action = action;
 
                     if (title == null) this.title = action;
                     else this.title = title;
-
-                    if (url != null) this.url = url;
 
                     this.icon = icon;
                 }
@@ -43,7 +40,6 @@ namespace Nyan.Modules.Web.Push.Google.Notification
                 public string action { get; set; }
                 public string title { get; set; }
                 public string icon { get; set; }
-                public string url { get; set; }
             }
         }
     }
