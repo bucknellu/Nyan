12a20b281ef920ae0611bd9f368060b5cf05cb06
Leo Botinelly <leo.botinelly@bucknell.edu>
2016-12-01 09:00:31 -0500

    Global WebAPI error handler

---
+++ Modules/Web/REST/GlobalErrorHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http.ExceptionHandling;
+using Nyan.Core.Settings;
+
+namespace Nyan.Modules.Web.REST
+{
+    public class GlobalErrorHandler : ExceptionLogger
+    {
+        public override void Log(ExceptionLoggerContext context)
+        {
+            Current.Log.Add(context.ExceptionContext.Exception);
+        }
+    }
+}
