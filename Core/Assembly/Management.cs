f346997124bf0c6dd69ea4d52862c9755bc2a401
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-01-19 10:48:34 -0500

    Flair

--- Core/Assembly/Management.cs
+++ Core/Assembly/Management.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
@@ -19,12 +18,24 @@ namespace Nyan.Core.Assembly
     /// </summary>
     public static class Management
     {
-        public static readonly Dictionary<string, System.Reflection.Assembly> AssemblyCache = new Dictionary<string, System.Reflection.Assembly>();
+        public static readonly Dictionary<string, System.Reflection.Assembly> AssemblyCache =
+            new Dictionary<string, System.Reflection.Assembly>();
+
         private static readonly Dictionary<Type, List<Type>> InterfaceClassesCache = new Dictionary<Type, List<Type>>();
         private static readonly object Lock = new object();
         private static readonly List<FileSystemWatcher> FsMonitors = new List<FileSystemWatcher>();
         private static readonly List<string> WatchedSources = new List<string>();
         public static readonly Dictionary<string, string> UniqueAssemblies = new Dictionary<string, string>();
+
+        private static readonly List<string> MonitorWhiteList = new List<string>
+        {
+            "InstallUtil.InstallLog",
+            "*.InstallLog",
+            "*.InstallState"
+        };
+
+        public static readonly Dictionary<Type, List<Type>> GetGenericsByBaseClassCache =
+            new Dictionary<Type, List<Type>>();
 
         static Management()
         {
@@ -33,8 +44,7 @@ namespace Nyan.Core.Assembly
 
             var targetScDir = Configuration.DataDirectory + "\\sc";
 
-            if (!Directory.Exists(targetScDir))
-                Directory.CreateDirectory(targetScDir);
+            if (!Directory.Exists(targetScDir)) Directory.CreateDirectory(targetScDir);
 
             AppDomain.CurrentDomain.SetCachePath(Configuration.DataDirectory + "\\sc");
 
@@ -44,8 +54,7 @@ namespace Nyan.Core.Assembly
 
             var self = System.Reflection.Assembly.GetEntryAssembly();
 
-            if (self != null)
-                AssemblyCache.Add(self.ToString(), self);
+            if (self != null) AssemblyCache.Add(self.ToString(), self);
 
             // 1st cycle: Local (base directory) assemblies
 
@@ -54,11 +63,14 @@ namespace Nyan.Core.Assembly
             //2nd cycle: Directories/assemblies referenced by system
 
             //    First by process-specific variables...
-            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40", EnvironmentVariableTarget.Process));
+            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40",
+                EnvironmentVariableTarget.Process));
             //    then by user-specific variables...
-            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40", EnvironmentVariableTarget.User));
+            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40",
+                EnvironmentVariableTarget.User));
             //    and finally system-wide variables.
-            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40", EnvironmentVariableTarget.Machine));
+            LoadAssembliesFromDirectory(Environment.GetEnvironmentVariable("nyan_ac_net40",
+                EnvironmentVariableTarget.Machine));
 
             //Now try to load:
 
@@ -75,8 +87,7 @@ namespace Nyan.Core.Assembly
 
                     errCount = 0;
 
-                    try
-                    {
+                    try {
                         item.Value.GetTypes();
                     }
                     catch (Exception)
@@ -90,8 +101,7 @@ namespace Nyan.Core.Assembly
             }
             Modules.Log.System.Add("Loaded modules: ");
 
-            foreach (var item in AssemblyCache)
-                Modules.Log.System.Add("    " + item.Value.Location + " (" + item.Value + ")");
+            foreach (var item in AssemblyCache) Modules.Log.System.Add("    " + item.Value.Location + " (" + item.Value + ")");
 
             Modules.Log.System.Add("Fatal Errors: " + errCount);
         }
@@ -104,18 +114,15 @@ namespace Nyan.Core.Assembly
 
         private static System.Reflection.Assembly CurrentDomain_AssemblyResolve(object sender, ResolveEventArgs args)
         {
-            if (args.RequestingAssembly != null)
-                Modules.Log.System.Add("        " + args.RequestingAssembly.FullName + ": Resolution request");
+            if (args.RequestingAssembly != null) Modules.Log.System.Add("        " + args.RequestingAssembly.FullName + ": Resolution request");
 
             Modules.Log.System.Add("            Resolving " + args.Name);
             var shortName = args.Name.Split(',')[0];
 
             var probe = GetAssemblyByName(shortName);
 
-            if (probe == null)
-                Modules.Log.System.Add("            [ERR] NOT FOUND");
-            else
-                Modules.Log.System.Add("            OK      : " + probe);
+            if (probe == null) Modules.Log.System.Add("            [ERR] NOT FOUND");
+            else Modules.Log.System.Add("            OK      : " + probe);
             return probe;
         }
 
@@ -127,8 +134,7 @@ namespace Nyan.Core.Assembly
             {
                 var list = path.Split(";".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).ToList();
 
-                foreach (var item in list)
-                    LoadAssembliesFromDirectory(item);
+                foreach (var item in list) LoadAssembliesFromDirectory(item);
             }
             else
             {
@@ -170,18 +176,11 @@ namespace Nyan.Core.Assembly
             }
         }
 
-        static List<string> MonitorWhiteList = new List<string>()
-        {
-            "InstallUtil.InstallLog",
-            "*.InstallLog",
-            "*.InstallState",
-        };
-
         public static string WildcardToRegex(string pattern)
         {
             return "^" + Regex.Escape(pattern).
-            Replace("\\*", ".*").
-            Replace("\\?", ".") + "$";
+                       Replace("\\*", ".*").
+                       Replace("\\?", ".") + "$";
         }
 
 
@@ -191,25 +190,15 @@ namespace Nyan.Core.Assembly
 
             var name = Path.GetFileName(e.FullPath);
 
-
             foreach (var i in MonitorWhiteList)
-            {
                 if (i.IndexOf("*", StringComparison.Ordinal) != -1)
                 {
                     var match = i.Replace("*.", "");
 
                     //TODO: Improve wildcard detection
-                    if (name.IndexOf(match) > -1)
-                    {
-                        return;
-                    }
-
+                    if (name.IndexOf(match) > -1) return;
                 }
-                else if (i.Equals(name))
-                {
-                    return;
-                }
-            }
+                else if (i.Equals(name)) { return; }
 
             // No need for system monitors anymore, better to interrupt and dispose all of them.
             foreach (var i in FsMonitors)
@@ -226,14 +215,17 @@ namespace Nyan.Core.Assembly
             Modules.Log.System.Add("[" + e.FullPath + "]: Change detected", Message.EContentType.ShutdownSequence);
 
             //For Web apps
-            try { HttpRuntime.UnloadAppDomain(); } catch { }
+            try {
+                HttpRuntime.UnloadAppDomain();
+            }
+            catch {}
 
             //For WinForm apps
             try
             {
                 //Application.Restart(); Environment.Exit(0);
             }
-            catch { }
+            catch {}
         }
 
         private static void LoadAssemblyFromPath(string path)
@@ -249,8 +241,7 @@ namespace Nyan.Core.Assembly
                 UniqueAssemblies.Add(p, path);
 
                 var assy = System.Reflection.Assembly.LoadFrom(path);
-                if (!AssemblyCache.ContainsKey(assy.ToString()))
-                    AssemblyCache.Add(assy.ToString(), assy);
+                if (!AssemblyCache.ContainsKey(assy.ToString())) AssemblyCache.Add(assy.ToString(), assy);
             }
             catch (Exception e)
             {
@@ -261,14 +252,10 @@ namespace Nyan.Core.Assembly
                     var typeLoadException = exception;
                     var loaderExceptions = typeLoadException.LoaderExceptions.ToList();
 
-                    if (loaderExceptions.Count > 0)
-                        Modules.Log.System.Add("    Fail " + path + ": " + loaderExceptions[0].Message);
-                    else
-
-                        Modules.Log.System.Add("    Fail " + path + ": Undefined.");
+                    if (loaderExceptions.Count > 0) Modules.Log.System.Add("    Fail " + path + ": " + loaderExceptions[0].Message);
+                    else Modules.Log.System.Add("    Fail " + path + ": Undefined.");
                 }
-                else
-                    Modules.Log.System.Add("    Fail " + path + ": " + e.Message);
+                else Modules.Log.System.Add("    Fail " + path + ": " + e.Message);
             }
         }
 
@@ -278,23 +265,20 @@ namespace Nyan.Core.Assembly
 
             var assySource = new List<System.Reflection.Assembly>();
 
-            if (limitToMainAssembly)
-                assySource.Add(Configuration.ApplicationAssembly);
-            else
-                assySource = AssemblyCache.Values.ToList();
+            if (limitToMainAssembly) assySource.Add(Configuration.ApplicationAssembly);
+            else assySource = AssemblyCache.Values.ToList();
 
             foreach (var asy in assySource)
-            {
                 classCol.AddRange(asy
                     .GetTypes()
                     .Where(type => type.BaseType != null)
-                    .Where(type => (type.BaseType.IsGenericType && type.BaseType.GetGenericTypeDefinition() == refType) || type.BaseType == refType));
-            }
+                    .Where(
+                        type =>
+                            (type.BaseType.IsGenericType && (type.BaseType.GetGenericTypeDefinition() == refType))
+                            || (type.BaseType == refType)));
 
             return classCol;
         }
-
-        public static readonly Dictionary<Type, List<Type>> GetGenericsByBaseClassCache = new Dictionary<Type, List<Type>>();
 
         public static List<Type> GetGenericsByBaseClass(Type refType)
         {
@@ -305,7 +289,6 @@ namespace Nyan.Core.Assembly
             try
             {
                 foreach (var asy in AssemblyCache.Values.ToList())
-                {
                     foreach (var st in asy.GetTypes())
                     {
                         if (st.BaseType == null) continue;
@@ -314,20 +297,14 @@ namespace Nyan.Core.Assembly
 
                         try
                         {
-                            foreach (var gta in st.BaseType.GenericTypeArguments)
-                            {
-                                if (gta == refType) classCol.Add(st);
-                            }
+                            foreach (var gta in st.BaseType.GenericTypeArguments) if (gta == refType) classCol.Add(st);
                         }
-                        catch { }
+                        catch {}
                     }
-                }
 
                 GetGenericsByBaseClassCache.Add(refType, classCol);
-
             }
-            catch (Exception e)
-            {
+            catch (Exception e) {
                 Current.Log.Add(e);
             }
 
@@ -350,8 +327,7 @@ namespace Nyan.Core.Assembly
                 var type = typeof(T);
                 var preRet = new List<Type>();
 
-                if (InterfaceClassesCache.ContainsKey(type))
-                    return InterfaceClassesCache[type];
+                if (InterfaceClassesCache.ContainsKey(type)) return InterfaceClassesCache[type];
 
                 Modules.Log.System.Add("Scanning for " + type);
 
@@ -361,8 +337,7 @@ namespace Nyan.Core.Assembly
 
                     Type[] preTypes;
 
-                    try
-                    {
+                    try {
                         preTypes = item.GetTypes();
                     }
                     catch (Exception e)
@@ -372,14 +347,10 @@ namespace Nyan.Core.Assembly
                             var typeLoadException = e as ReflectionTypeLoadException;
                             var loaderExceptions = typeLoadException.LoaderExceptions.ToList();
 
-                            if (loaderExceptions.Count > 0)
-                                Modules.Log.System.Add("    Fail " + item + ": " + loaderExceptions[0].Message);
-                            else
-
-                                Modules.Log.System.Add("    Fail " + item + ": Undefined.");
+                            if (loaderExceptions.Count > 0) Modules.Log.System.Add("    Fail " + item + ": " + loaderExceptions[0].Message);
+                            else Modules.Log.System.Add("    Fail " + item + ": Undefined.");
                         }
-                        else
-                            Modules.Log.System.Add("    Fail " + item + ": " + e.Message);
+                        else Modules.Log.System.Add("    Fail " + item + ": " + e.Message);
                         // Well, this loading can fail by a (long) variety of reasons. 
                         // It's not a real problem not to catch exceptions here. 
                         continue;
@@ -403,19 +374,16 @@ namespace Nyan.Core.Assembly
 
                     var attrs = item.GetCustomAttributes(typeof(PriorityAttribute), true);
 
-                    if (attrs.Length > 0)
-                        level = ((PriorityAttribute)attrs[0]).Level;
+                    if (attrs.Length > 0) level = ((PriorityAttribute) attrs[0]).Level;
 
                     priorityList.Add(new KeyValuePair<int, Type>(level, item));
                 }
 
                 priorityList.Sort((firstPair, nextPair) => nextPair.Key - firstPair.Key);
 
-                foreach (var item in priorityList) { Modules.Log.System.Add("        " + item.Key + " " + item.Value.Name); }
+                foreach (var item in priorityList) Modules.Log.System.Add("        " + item.Key + " " + item.Value.Name);
 
                 var ret = priorityList.Select(item => item.Value).ToList();
-
-
 
                 InterfaceClassesCache.Add(type, ret); // Caching results, so similar queries will return from cache
 
--- Modules/Web/REST/Sequences.cs
+++ Modules/Web/REST/Sequences.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Web;
 using System.Web.Http;
 using System.Web.Http.Dispatcher;
 using Nyan.Core.Assembly;
+using Nyan.Core.Settings;
 
 namespace Nyan.Modules.Web.REST
 {
@@ -13,17 +15,18 @@ namespace Nyan.Modules.Web.REST
     /// </summary>
     public static class Sequences
     {
+
         /// <summary>
         ///     Start-up sequence hook for the rest module. Register and initializes WebApi routes.
         /// </summary>
         public static void Start()
         {
-            var apictrls = Management.GetClassesByBaseClass(typeof (ApiController)).ToList();
+            var apictrls = Management.GetClassesByBaseClass(typeof(ApiController)).ToList();
             Core.Modules.Log.System.Add(apictrls.Count + " WebApi Controller classes loaded");
 
 
             // Replaces the default resolver: Forces load of ApiControllers even if they're in a remote assembly
-            GlobalConfiguration.Configuration.Services.Replace(typeof (IAssembliesResolver), new GlobalAssemblyResolver());
+            GlobalConfiguration.Configuration.Services.Replace(typeof(IAssembliesResolver), new GlobalAssemblyResolver());
 
             GlobalConfiguration.Configure(Initialization.Register);
 
@@ -40,17 +43,17 @@ namespace Nyan.Modules.Web.REST
         /// </summary>
         public static void End()
         {
-            var runtime = (HttpRuntime) typeof (HttpRuntime).InvokeMember("_theRuntime",
+            var runtime = (HttpRuntime)typeof(HttpRuntime).InvokeMember("_theRuntime",
                 BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetField,
                 null, null, null);
 
             if (runtime != null)
             {
-                var shutDownMessage = (string) runtime.GetType().InvokeMember("_shutDownMessage",
+                var shutDownMessage = (string)runtime.GetType().InvokeMember("_shutDownMessage",
                     BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField,
                     null, runtime, null);
 
-                var shutDownStack = (string) runtime.GetType().InvokeMember("_shutDownStack",
+                var shutDownStack = (string)runtime.GetType().InvokeMember("_shutDownStack",
                     BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.GetField,
                     null, runtime, null);
 
