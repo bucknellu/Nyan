8e9e06660d5c5840219cd029dd1f0d01f6a39428
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-04-19 13:27:38 -0400

    Cleanup

--- Modules/Data/MongoDB/MongoDbAdapter.cs
+++ Modules/Data/MongoDB/MongoDbAdapter.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Data.Common;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using Nyan.Core.Extensions;
-using Nyan.Core.Modules.Data;
+﻿using System.Data.Common;
 using Nyan.Core.Modules.Data.Adapter;
-using Nyan.Core.Modules.Data.Maintenance;
-using Nyan.Core.Modules.Log;
-using Nyan.Core.Settings;
-using Nyan.Core.Wrappers;
 
 namespace Nyan.Modules.Data.MongoDB
 {
@@ -20,7 +8,7 @@ namespace Nyan.Modules.Data.MongoDB
         public MongoDbAdapter()
         {
             useOutputParameterForInsertedKeyExtraction = true;
-                //Some DBs may require an OUT parameter to extract the new ID. Not the case here.
+            //Some DBs may require an OUT parameter to extract the new ID. Not the case here.
             sqlTemplateInsertSingleWithReturn =
                 "INSERT INTO {0} ({1}) VALUES ({2}) RETURNING CAST({3} AS VARCHAR2(38) ) INTO {4}newid";
             sqlTemplateTableTruncate = "TRUNCATE TABLE {0}";
@@ -28,14 +16,12 @@ namespace Nyan.Modules.Data.MongoDB
 
             dynamicParameterType = typeof(MongoDbDynamicParameters);
 
-
             Interceptor = new MongoDbinterceptor(this);
         }
 
         public override void RenderSchemaEntityNames<T>() { }
         public override DbConnection Connection(string connectionString) { return null; }
 
-        public override void CheckDatabaseEntities<T>() {}
-
+        public override void CheckDatabaseEntities<T>() { }
     }
 }
