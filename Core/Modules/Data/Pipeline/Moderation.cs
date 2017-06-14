fa14c92947aa162140a46599c245b1801897a6b8
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-06-14 09:20:38 -0400

    Moderation pipeline stub

---
+++ Core/Modules/Data/Pipeline/Moderation.cs
@@ -0,0 +1,7 @@
+﻿namespace Nyan.Core.Modules.Data.Pipeline
+{
+    public class Moderation : IBeforeActionPipeline
+    {
+        public T Process<T>(Support.EAction action, T current, T source) where T : MicroEntity<T> { return current; }
+    }
+}
