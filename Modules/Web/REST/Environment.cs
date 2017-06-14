987598e57a71e78ba197fc07a352db0d1ba53bff
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-06-14 09:16:57 -0400

    Global helper for base Url

---
+++ Modules/Web/REST/Environment.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+
+namespace Nyan.Modules.Web.REST
+{
+    public static class Environment
+    {
+        public static string BaseUrl
+        {
+            get
+            {
+                var request = HttpContext.Current.Request;
+                var baseUrl = request.Url.Scheme + "://" +
+                              request.Url.Authority +
+                              request.ApplicationPath.TrimEnd('/') + "/";
+                return baseUrl;
+            }
+        }
+    }
+}
