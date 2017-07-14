287c44b12a9e045a7d4079efb7210422b83719f7
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-07-14 10:20:18 -0400

    PriorityAttribute can now be applied to Maintenance Methods

--- Core/Shared/PriorityAttribute.cs
+++ Core/Shared/PriorityAttribute.cs
@@ -2,7 +2,7 @@
 
 namespace Nyan.Core.Shared
 {
-    [AttributeUsage(AttributeTargets.Class)]
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
     public sealed class PriorityAttribute : Attribute
     {
         public int Level { get; set; }
