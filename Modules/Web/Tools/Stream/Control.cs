c9641f82f1332e2ce9c5498d3b44cfb5564fdc7d
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-07-14 10:24:01 -0400

    Stub stream control

---
+++ Modules/Web/Tools/Stream/Control.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Nyan.Modules.Web.Tools.Stream
+{
+    public static class Extensions
+    {
+        public static IEnumerable<object> ControlFlow(this IEnumerable<object> ol, int sliceSize = 64, int sliceIndex = 0)
+        {
+
+            if (HttpContext.Current.Request.QueryString["i"] != null) sliceIndex = int.Parse(HttpContext.Current.Request.QueryString["i"]);
+            if (HttpContext.Current.Request.QueryString["s"] != null) sliceSize = int.Parse(HttpContext.Current.Request.QueryString["s"]);
+
+            var skipCount = sliceIndex * sliceSize;
+
+            var buffer = ol
+                .Skip(skipCount)
+                .Take(sliceSize);
+
+
+
+            return buffer;
+        }
+    }
+}
