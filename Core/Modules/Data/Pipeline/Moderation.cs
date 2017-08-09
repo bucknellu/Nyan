d7b67373d1eb2966813fd2bc382765e47334b4a6
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-08-09 16:23:57 -0400

    Cleanup

--- Core/Modules/Data/Pipeline/Moderation.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Nyan.Core.Modules.Data.Pipeline
-{
-    public class Moderation : IBeforeActionPipeline
-    {
-        public T Process<T>(Support.EAction action, T current, T source) where T : MicroEntity<T> { return current; }
-    }
-}
