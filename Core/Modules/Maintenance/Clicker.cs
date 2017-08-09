f315306a9d8270c9bf8a579ddc3b72c36f6275fb
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-08-09 16:23:48 -0400

    Info dump on End(), flair

--- Core/Modules/Maintenance/Clicker.cs
+++ Core/Modules/Maintenance/Clicker.cs
@@ -7,19 +7,20 @@ namespace Nyan.Core.Modules.Maintenance
 {
     public class Clicker
     {
-        private long _pCount;
         private long _pIndex;
         private string _pMessage;
         private int _pNotifySlice;
 
         private Stopwatch _s;
 
+        public long Count { get; private set; }
+
         public Clicker(string pMessage, long pCount) { Start(pMessage, pCount); }
 
         public void Start(string pMessage, long pCount, int pNotifySlice = 100)
         {
             _pMessage = pMessage;
-            _pCount = pCount;
+            Count = pCount;
             _pNotifySlice = pNotifySlice;
 
             _s = new Stopwatch();
@@ -33,10 +34,19 @@ namespace Nyan.Core.Modules.Maintenance
             _pIndex++;
             if (_pIndex % _pNotifySlice != 0) return;
 
-            var part = (double) _pIndex / _pCount;
+            var part = (double)_pIndex / Count;
             var invPart = TimeSpan.FromMilliseconds(_s.ElapsedMilliseconds * (1 / part));
 
-            Current.Log.Add($"    {_pMessage}: {_pIndex}/{_pCount} ({part:P2} / {invPart.Subtract(_s.Elapsed)} left, ~{invPart} Total)", Message.EContentType.MoreInfo);
+            Current.Log.Add($"    {_pMessage}: {_pIndex}/{Count} ({part:P2} / {invPart.Subtract(_s.Elapsed)} left, ~{invPart} Total)", Message.EContentType.MoreInfo);
+        }
+
+        public void End()
+        {
+            _s.Stop();
+
+            var regPerSec = Count / ((double)_s.ElapsedMilliseconds / 1000);
+
+            Current.Log.Add($"    {_pMessage}: END ({_s.Elapsed} elapsed, {regPerSec:F2} items/sec)", Message.EContentType.Info);
         }
     }
 }
