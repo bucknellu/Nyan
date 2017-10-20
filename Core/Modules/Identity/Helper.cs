e4c3a66a77d9b1d30bd9c99cf68ccc6734fa1f30
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-10-20 15:08:46 -0400

    Identity helper for permission split

---
+++ Core/Modules/Identity/Helper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Nyan.Core.Settings;
+
+namespace Nyan.Core.Modules.Identity
+{
+    public static class Helper
+    {
+        public static bool HasAnyPermissions(string perm)
+        {
+            if (perm == null) return true;
+            if (perm == "") return true;
+
+            var separator = ',';
+
+            if (perm.IndexOf(";", StringComparison.Ordinal) != -1) separator = ';';
+            if (perm.IndexOf(":", StringComparison.Ordinal) != -1) separator = ':';
+
+            while (perm.IndexOf(" ", StringComparison.Ordinal) != -1) perm = perm.Replace(" ", ""); // Eliminates spaces
+
+            var queryPerms = perm.Split(separator);
+
+            return queryPerms.Any(s => Current.Authorization.CheckPermission(s));
+        }
+    }
+}
