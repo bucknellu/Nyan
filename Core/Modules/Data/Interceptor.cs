0addb62cbbcbbe90fe3a1f012c0fcbf9a7f75b15
Leo Botinelly <leo.botinelly@bucknell.edu>
2017-10-20 15:07:33 -0400

    Generalized Query and GetAll methods

--- Core/Modules/Data/Interceptor.cs
+++ Core/Modules/Data/Interceptor.cs
@@ -10,10 +10,10 @@ namespace Nyan.Core.Modules.Data
             StaticArray
         }
     }
+
     public interface IInterceptor
     {
         void Connect<T>(string statementsConnectionString, ConnectionBundlePrimitive bundle) where T : MicroEntity<T>;
-        List<T> Get<T>() where T : MicroEntity<T>;
         T Get<T>(string locator) where T : MicroEntity<T>;
         string Save<T>(MicroEntity<T> microEntity) where T : MicroEntity<T>;
         void Remove<T>(string locator) where T : MicroEntity<T>;
@@ -21,12 +21,17 @@ namespace Nyan.Core.Modules.Data
         void RemoveAll<T>() where T : MicroEntity<T>;
         void Insert<T>(MicroEntity<T> microEntity) where T : MicroEntity<T>;
         List<T> Query<T>(string sqlStatement, object rawObject) where T : MicroEntity<T>;
-        List<T> Get<T>(MicroEntityParametrizedGet parm) where T : MicroEntity<T>;
+        List<TU> Query<T, TU>(string statement, object rawObject, InterceptorQuery.EType ptype) where T : MicroEntity<T>;
+        List<T> GetAll<T>(string extraParms = null) where T : MicroEntity<T>;
+        List<TU> GetAll<T, TU>(string extraParms = null) where T : MicroEntity<T>;
+        List<T> GetAll<T>(MicroEntityParametrizedGet parm, string extraParms = null) where T : MicroEntity<T>;
+        List<TU> GetAll<T, TU>(MicroEntityParametrizedGet parm, string extraParms = null) where T : MicroEntity<T>;
         void Initialize<T>() where T : MicroEntity<T>;
         long RecordCount<T>() where T : MicroEntity<T>;
+        long RecordCount<T>(string extraParms) where T : MicroEntity<T>;
         long RecordCount<T>(MicroEntityParametrizedGet qTerm) where T : MicroEntity<T>;
+        long RecordCount<T>(MicroEntityParametrizedGet qTerm, string extraParms) where T : MicroEntity<T>;
         List<T> ReferenceQueryByField<T>(string field, string id) where T : MicroEntity<T>;
         List<T> ReferenceQueryByField<T>(object query) where T : MicroEntity<T>;
-        List<TU> Query<TU>(string statement, object rawObject, InterceptorQuery.EType ptype);
     }
 }
