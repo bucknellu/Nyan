d852410bff939b25cdba15987852111838a977ba
Leo Botinelly <leo.botinelly@bucknell.edu>
2018-03-08 11:18:53 -0500

    PaginatedControlFlow

--- Modules/Web/Tools/Stream/Control.cs
+++ Modules/Web/Tools/Stream/Control.cs
@@ -18,9 +18,30 @@ namespace Nyan.Modules.Web.Tools.Stream
                 .Skip(skipCount)
                 .Take(sliceSize);
 
-
-
             return buffer;
         }
+
+        public static object PaginatedControlFlow(this IEnumerable<object> ol, int sliceSize = 64, int sliceIndex = 0)
+        {
+            if (ol == null) return null;
+
+            if (HttpContext.Current.Request.QueryString["i"] != null) sliceIndex = int.Parse(HttpContext.Current.Request.QueryString["i"]);
+            if (HttpContext.Current.Request.QueryString["s"] != null) sliceSize = int.Parse(HttpContext.Current.Request.QueryString["s"]);
+
+            var skipCount = sliceIndex * sliceSize;
+
+            var eol = ol.ToList();
+
+            var ec = eol.Count;
+
+            var buffer = eol
+                .Skip(skipCount)
+                .Take(sliceSize);
+
+            var retObj = new {data = buffer, index = sliceIndex, size = sliceSize, count = ec, pages = (int)((ec / sliceSize) + 1)};
+
+            return retObj;
+        }
+
     }
 }
