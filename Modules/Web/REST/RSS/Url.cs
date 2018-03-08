20316d1c03fc35b9d7158d116f8621471b252a48
Leo Botinelly <leo.botinelly@bucknell.edu>
2018-03-08 11:24:15 -0500

    RSS model

---
+++ Modules/Web/REST/RSS/Url.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Nyan.Modules.Web.REST.RSS {
+    public class Url
+    {
+        public int UrlId { get; set; }
+        public string Address { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+    }
+}
