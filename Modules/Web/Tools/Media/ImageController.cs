ceff56dcb460d3ca07ed853ddfbca2cfe5ff47e6
Leo Botinelly <lmbs001@bucknell.edu>
2018-06-14 11:31:33 -0400

    Proxy revamp

--- Modules/Web/Tools/Media/ImageController.cs
+++ Modules/Web/Tools/Media/ImageController.cs
@@ -9,15 +9,24 @@ using System.Net.Http.Headers;
 using System.Web;
 using System.Web.Http;
 using Nyan.Core.Extensions;
+using Nyan.Core.Media;
 using Nyan.Core.Settings;
+using Nyan.Modules.Web.REST;
 
 namespace Nyan.Modules.Web.Tools.Media
 {
     [RoutePrefix("stack/tools/image")]
     public class ImageController : ApiController
     {
+        public virtual HttpResponseMessage GetReference([FromUri] string url, [FromUri] int? width = null, [FromUri] int? height = null) { return GetReference(url, width, height, true); }
+
         [Route("external"), HttpGet]
-        public virtual HttpResponseMessage GetReference([FromUri] string url, [FromUri] int? width = null, [FromUri] int? height = null)
+        public virtual HttpResponseMessage GetReference([FromUri] string url,
+                                                        [FromUri] int? width = null, [FromUri] int? height = null,
+                                                        [FromUri] bool crop = true,
+                                                        [FromUri] int? w = null, [FromUri] int? h = null,
+                                                        [FromUri] string format = null, [FromUri] string f = null,
+                                                        [FromUri] string position = null, [FromUri] string p = null)
         {
 
             var redirects = new[] { ".svg" };
@@ -34,93 +43,13 @@ namespace Nyan.Modules.Web.Tools.Media
                 }
             }
 
-
-            return InternalGetReference(url, width, height);
+            return InternalGetReference(url, w ?? width, h ?? height, crop, f ?? format, p ?? position);
         }
 
-        public virtual HttpResponseMessage InternalGetReference(string url, int? width = null, int? height = null)
+
+        public virtual HttpResponseMessage InternalGetReference(string url, int? width = null, int? height = null, bool crop = true, string format = null, string position = null)
         {
-            Uri uriResult;
-
-            if (url.IndexOf("http", StringComparison.Ordinal) == -1) url = string.Format("http://{0}", url);
-
-            //can contain the text http and contain incorrect uri scheme.
-            var isUrl = Uri.TryCreate(url, UriKind.Absolute, out uriResult) && (uriResult.Scheme == Uri.UriSchemeHttp || uriResult.Scheme == Uri.UriSchemeHttps);
-
-            if (!isUrl) throw new ArgumentException("Parameter is invalid: url");
-
-            var cacheDir = Core.Configuration.DataDirectory + "\\cache";
-
-            Image image;
-
-            var imgConverter = new ImageConverter();
-
-            var dimensionsPart = "";
-
-            if (width != null) dimensionsPart += "w" + width;
-
-            if (height != null) dimensionsPart += "h" + height;
-
-            var outtype = MimeMapping.GetMimeMapping(url);
-
-            if (outtype == "application/octet-stream") outtype = "image/png";
-
-            var md5Url = url.Md5Hash();
-            var preCompName = cacheDir + "\\media-external-" + md5Url + "-" + dimensionsPart + ".png";
-
-            Current.Log.Add(outtype + " : " + preCompName);
-
-            if (!Directory.Exists(cacheDir)) Directory.CreateDirectory(cacheDir);
-
-            if (File.Exists(preCompName))
-            {
-                image = new Bitmap(preCompName);
-                outtype = "image/png";
-            }
-            else
-            {
-                //connect via web client and get the photo at the specified url
-
-                Current.Log.Add("Fetching external resource: " + url);
-
-                var httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-
-                using (var httpWebReponse = (HttpWebResponse)httpWebRequest.GetResponse())
-                {
-                    using (var stream = httpWebReponse.GetResponseStream())
-                    {
-                        image = Image.FromStream(stream);
-
-                        if (image == null) throw new FileNotFoundException(url + " could not be found");
-
-                        if (width == null) width = image.Width;
-
-                        if (height == null) height = image.Height;
-
-                        if ((int)height * (int)width > 2560000)
-                            throw new ArgumentException(
-                                "Combined size is invalid. Limit pixel count to 2560000 (width x height).");
-
-                        //resize image
-                        if (width != image.Width || height != image.Height)
-                        {
-                            image = Utilities.ResizeImage(image, (int)width, (int)height);
-                            Current.Log.Add("Caching compiled image: " + preCompName);
-                            image.Save(preCompName, ImageFormat.Png);
-                        }
-                    }
-                }
-            }
-
-            var imageBytes = (byte[])imgConverter.ConvertTo(image, typeof(byte[]));
-
-            var result = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayContent(imageBytes) };
-
-            result.Headers.CacheControl = new CacheControlHeaderValue();
-            result.Content.Headers.Add("Expires", DateTime.Now.AddDays(3).ToUniversalTime().ToString("R"));
-            result.Content.Headers.ContentType = new MediaTypeHeaderValue(outtype);
-
-            return result;
+            return Utilities.GetFormattedImageResourcePath(url, width, height, crop, format, position).FromPathToResponseMessage();
         }
 
         [Route("external/hash"), HttpGet]
