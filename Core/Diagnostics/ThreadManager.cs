8728a373a88be40a27c2d2609aaf54dce70e27a0
Leo Botinelly <lmbs001@bucknell.edu>
2018-06-14 11:28:58 -0400

    Debug: Thread helper

---
+++ Core/Diagnostics/ThreadManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+using Nyan.Core.Extensions;
+
+namespace Nyan.Core.Diagnostics
+{
+    public class ThreadHelper
+    {
+        private static readonly ThreadLocal<string> _Uid = new ThreadLocal<string>(() => Guid.NewGuid().ToShortGuid().ToString());
+        public static string Uid => _Uid.Value;
+    }
+}
