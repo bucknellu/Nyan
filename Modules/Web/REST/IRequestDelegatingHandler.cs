c5db0af6d464c02dd48d506ea3ce42bfe1d577bc
Leo Botinelly <lmbs001@bucknell.edu>
2018-06-14 11:26:01 -0400

    Generic delegateHandler for WebAPI calls

---
+++ Modules/Web/REST/IRequestDelegatingHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+
+namespace Nyan.Modules.Web.REST
+{
+    public interface IRequestDelegatingHandler
+    {
+        void OnRequest(HttpRequestMessage request, string requestBody, string responseBody);
+    }
+}
---
+++ Modules/Web/REST/Instances.cs
@@ -0,0 +1,30 @@
+﻿using System.Net.Http;
+using Nyan.Core.Assembly;
+using Nyan.Core.Extensions;
+
+namespace Nyan.Modules.Web.REST
+{
+    public static class Instances
+    {
+        private static DelegatingHandler _requestDelegatingHandlerInstance;
+
+        private static readonly object Padlock = new object();
+
+        public static DelegatingHandler DelegatingHandler
+        {
+            get
+            {
+                if (_requestDelegatingHandlerInstance != null) return _requestDelegatingHandlerInstance;
+
+                lock (Padlock)
+                {
+                    if (_requestDelegatingHandlerInstance != null) return _requestDelegatingHandlerInstance;
+
+                    //There'll be at least one, the default.
+                    _requestDelegatingHandlerInstance = Management.GetClassesByInterface<IRequestDelegatingHandler>()[0].CreateInstance<DelegatingHandler>();
+                    return _requestDelegatingHandlerInstance;
+                }
+            }
+        }
+    }
+}
