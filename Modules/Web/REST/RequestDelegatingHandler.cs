dc83da2853e93d793bf076863a984aa9913d37df
Leo Botinelly <lmbs001@bucknell.edu>
2018-06-14 11:31:25 -0400

    Generic WebAPI exception/proc handler

---
+++ Modules/Web/REST/RequestDelegatingHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Nyan.Core.Settings;
+using Nyan.Core.Shared;
+
+namespace Nyan.Modules.Web.REST
+{
+    // Need to log asp.net webapi 2 request and response body to a database
+    // https://stackoverflow.com/a/23660832/1845714
+
+    /// <inheritdoc />
+    [Priority(Level = -99)]
+    public class RequestDelegatingHandler : DelegatingHandler, IRequestDelegatingHandler
+    {
+        public virtual void OnRequest(HttpRequestMessage request, string requestBody, string responseBody) { }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            // let other handlers process the request
+            var result = await base.SendAsync(request, cancellationToken);
+
+            string responseBody = null;
+            if (result.Content != null) responseBody = await result.Content.ReadAsStringAsync();
+
+            // log request body
+            var requestBody = await request.Content.ReadAsStringAsync();
+
+            try { new Thread(() => OnRequest(request, requestBody, responseBody)).Start(); } catch (Exception e) { Current.Log.Add(e); }
+
+            return result;
+        }
+    }
+}
--- Modules/Web/REST/SimpleHttpResponseMessage.cs
+++ Modules/Web/REST/SimpleHttpResponseMessage.cs
@@ -1,11 +1,16 @@
 ﻿using System;
+using System.Drawing;
+using System.IO;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using Nyan.Core.Media;
 using Nyan.Core.Settings;
 
 namespace Nyan.Modules.Web.REST
 {
-    public class SimpleHttpResponseMessage: HttpResponseMessage
+    public class SimpleHttpResponseMessage : HttpResponseMessage
     {
         public SimpleHttpResponseMessage(Exception e)
         {
@@ -19,6 +24,36 @@ namespace Nyan.Modules.Web.REST
             StatusCode = code;
             Content = new StringContent(content);
         }
+    }
 
+    public static class Extensions
+    {
+        public static HttpResponseMessage ToResponseMessage(this Image image, double cacheHours = 24)
+        {
+            var imgConverter = new ImageConverter();
+            var imageBytes = (byte[])imgConverter.ConvertTo(image, typeof(byte[]));
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK) { Content = new ByteArrayContent(imageBytes) };
+
+            result.Headers.CacheControl = new CacheControlHeaderValue();
+            result.Content.Headers.Add("Expires", DateTime.Now.AddHours(cacheHours).ToUniversalTime().ToString("R"));
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue(image.GetCodec().MimeType);
+
+            return result;
+        }
+
+        public static HttpResponseMessage FromPathToResponseMessage(this string filePath, double cacheHours = 24)
+        {
+            var dataBytes = File.ReadAllBytes(filePath);
+            var dataStream = new MemoryStream(dataBytes);
+
+            var result = new HttpResponseMessage(HttpStatusCode.OK) { Content = new StreamContent(dataStream) };
+
+            result.Headers.CacheControl = new CacheControlHeaderValue();
+            result.Content.Headers.Add("Expires", DateTime.Now.AddHours(cacheHours).ToUniversalTime().ToString("R"));
+            result.Content.Headers.ContentType = new MediaTypeHeaderValue(MimeMapping.GetMimeMapping(filePath));
+
+            return result;
+        }
     }
 }
