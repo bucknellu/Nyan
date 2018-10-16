07ee633368f279a1a494e21e720ec11a991c146c
Leo Botinelly <lmbs001@bucknell.edu>
2018-10-16 16:19:17 -0400

    Push Notification primitives

---
+++ Modules/Web/Push/Google/Notification/INotificationPrimitive.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+
+namespace Nyan.Modules.Web.Push.Google.Notification
+{
+    public interface INotificationPrimitive
+    {
+        Options options { get; set; }
+        string title { get; set; }
+    }
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+    public class Options
+    {
+        public List<Action> actions = new List<Action>();
+        public string body { get; set; }
+        public string dir { get; set; } = "auto";
+        public string lang { get; set; }
+        public bool renotify { get; set; } = false;
+        public bool requireInteraction { get; set; } = false;
+        public string tag { get; set; }
+        public List<int> vibrate { get; set; } = new List<int>();
+        public Dictionary<string, string> data { get; set; } = new Dictionary<string, string>();
+        public string icon { get; set; }
+        public string image { get; set; }
+        public string badge { get; set; }
+        public class Action
+        {
+            public Action(string action, string title = null, string icon = null)
+            {
+                this.action = action;
+
+                if (title == null) this.title = action;
+                else this.title = title;
+
+                this.icon = icon;
+            }
+
+            public string action { get; set; }
+            public string title { get; set; }
+            public string icon { get; set; }
+        }
+    }
+
+}
--- Modules/Web/Push/Google/Notification/NotificationPrimitive.cs
+++ Modules/Web/Push/Google/Notification/NotificationPrimitive.cs
@@ -1,46 +1,12 @@
-﻿using System.Collections.Generic;
+﻿
 
 // ReSharper disable InconsistentNaming
 
 namespace Nyan.Modules.Web.Push.Google.Notification
 {
-    public class NotificationPrimitive
+    public class NotificationPrimitive : INotificationPrimitive
     {
-        public Options options = new Options();
-
-        // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
-
+        public Options options { get; set; }
         public string title { get; set; }
-        public class Options
-        {
-            public List<Action> actions = new List<Action>();
-            public string body { get; set; }
-            public string dir { get; set; } = "auto";
-            public string lang { get; set; }
-            public bool renotify { get; set; } = false;
-            public bool requireInteraction { get; set; } = false;
-            public string tag { get; set; }
-            public List<int> vibrate { get; set; } = new List<int>();
-            public Dictionary<string, string> data { get; set; } = new Dictionary<string, string>();
-            public string icon { get; set; }
-            public string image { get; set; }
-            public string badge { get; set; }
-            public class Action
-            {
-                public Action(string action, string title = null, string icon = null)
-                {
-                    this.action = action;
-
-                    if (title == null) this.title = action;
-                    else this.title = title;
-
-                    this.icon = icon;
-                }
-
-                public string action { get; set; }
-                public string title { get; set; }
-                public string icon { get; set; }
-            }
-        }
     }
 }
