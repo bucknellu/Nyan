a8d1335185d7a9a12dab3750590add0bb67767ec
Leo Botinelly <leo.botinelly@bucknell.edu>
2018-11-02 09:33:17 -0400

    Added ChangeLog support

--- Core/Modules/Maintenance/MaintenanceTaskResult.cs
+++ Core/Modules/Maintenance/MaintenanceTaskResult.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Collections.Generic;
+using Nyan.Core.Settings;
 
-namespace Nyan.Core.Modules.Maintenance {
+namespace Nyan.Core.Modules.Maintenance
+{
     public class MaintenanceTaskResult
     {
         public enum EResultStatus
@@ -19,5 +22,46 @@ namespace Nyan.Core.Modules.Maintenance {
         public DateTime TimeStamp { get; set; } = DateTime.Now;
         public TimeSpan Duration { get; set; } = TimeSpan.Zero;
         public int Priority { get; set; }
+
+        public List<ChangeEntry> Changes { get; set; }
+
+        public ChangeEntry AddChange(ChangeEntry.EChangeType type, string subject, string locator, string valueName = null, string originalValue = null, string newValue = null, string comments = null)
+        {
+            var entry = new ChangeEntry { Comments = comments, Locator = locator, Subject = subject, Type = type, NewValue = newValue, OriginalValue = originalValue, ValueName = valueName };
+
+            //switch (type) {
+            //    case ChangeEntry.EChangeType.CREATE:
+            //    case ChangeEntry.EChangeType.REMOVE: Current.Log.Add($"{type.ToString()} : {subject} [{locator}] {comments}");
+            //        break;
+            //    case ChangeEntry.EChangeType.MODIFY: Current.Log.Add($"{type.ToString()} : {subject} [{locator}] - {valueName} '{originalValue}' > '{newValue}'  {comments}");
+            //        break;
+            //    case ChangeEntry.EChangeType.OTHER: break;
+            //    default: throw new ArgumentOutOfRangeException(nameof(type), type, null);
+            //}
+
+            if (Changes == null) Changes = new List<ChangeEntry>();
+            Changes.Add(entry);
+
+            return entry;
+        }
+
+        public class ChangeEntry
+        {
+            public enum EChangeType
+            {
+                CREATE,
+                MODIFY,
+                REMOVE,
+                OTHER
+            }
+
+            public string Subject { get; set; }
+            public EChangeType Type { get; set; }
+            public string Locator { get; set; }
+            public string Comments { get; set; }
+            public string ValueName { get; set; }
+            public string OriginalValue { get; set; }
+            public string NewValue { get; set; }
+        }
     }
 }
