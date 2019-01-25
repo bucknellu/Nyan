7cef5bf1a025046da8e057711bdf575f09f98cd4
Leo Botinelly <leo.botinelly@bucknell.edu>
2019-01-25 14:56:57 -0500

    LocalStorage

---
+++ Core/Modules/Storage/LocalCache.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+
+namespace Nyan.Core.Modules.Storage
+{
+    public static class LocalCache
+    {
+        public static string BasePath => Configuration.DataDirectory + "\\cache\\local";
+
+        public static void Put(string name, string content)
+        {
+            if (!Directory.Exists(BasePath)) Directory.CreateDirectory(BasePath);
+
+            var file = BasePath + "\\" + name;
+
+            File.WriteAllText(file, content);
+        }
+
+        public static string Get(string name)
+        {
+            var file = BasePath + "\\" + name;
+            return !File.Exists(file) ? null : File.ReadAllText(file);
+        }
+
+        public static void Delete(string name)
+        {
+            var file = BasePath + "\\" + name;
+            if (File.Exists(file)) File.Delete(file);
+        }
+    }
+}
