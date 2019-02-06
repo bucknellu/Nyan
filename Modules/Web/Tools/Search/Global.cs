9ef63a06961ec07cfe79648af1554565a70c2535
Leo Botinelly <lmbs001@bucknell.edu>
2019-02-06 12:14:07 -0500

    Keymatch priority

--- Modules/Web/Tools/Search/Global.cs
+++ Modules/Web/Tools/Search/Global.cs
@@ -36,7 +36,7 @@ namespace Nyan.Modules.Web.Tools.Search
                 foreach (var searchableType in SearchableTypes)
                 {
                     step = "Creating instance of " + searchableType.FullName;
-                    var instance = (ISearch)Activator.CreateInstance(searchableType);
+                    var instance = (ISearch) Activator.CreateInstance(searchableType);
 
                     step = "Adding task for " + searchableType.FullName;
 
@@ -56,14 +56,13 @@ namespace Nyan.Modules.Web.Tools.Search
                 var retcon = new Dictionary<string, List<SearchResult>>();
 
                 foreach (var task in tasks)
-                {
                     if (!task.IsCompleted)
                     {
                         Current.Log.Add("Task Canceled: " + task.Result.Key);
                         var cts = new CancellationTokenSource();
                         var cancellableTask = task.ContinueWith(ignored => { }, cts.Token);
                         cts.Cancel();
-                        Task.WaitAny(new[] { cancellableTask }, TimeSpan.FromSeconds(1));
+                        Task.WaitAny(new[] {cancellableTask}, TimeSpan.FromSeconds(1));
                     }
                     else
                     {
@@ -73,7 +72,6 @@ namespace Nyan.Modules.Web.Tools.Search
                             var col2 = retcon[task.Result.Key];
                             retcon[task.Result.Key] = col1.Concat(col2).ToList();
 
-
                             Current.Log.Add("CONCAT [{0}]: {1} items ({2} ms)".format(task.Result.Key, retcon[task.Result.Key].Count, task.Result.Value.ProcessingTime));
                         }
                         else // Just add.
@@ -82,10 +80,20 @@ namespace Nyan.Modules.Web.Tools.Search
                             Current.Log.Add("ADD    [{0}]: {1} items ({2} ms)".format(task.Result.Key, task.Result.Value.Results.Count, task.Result.Value.ProcessingTime));
                         }
                     }
+
+                var sortCon = new Dictionary<string, List<SearchResult>>();
+
+                foreach (var i in retcon.ToList())
+                {
+                    sortCon[i.Key] = i.Value.Distinct(new SearchResult.Comparer()).OrderBy(j => !j.IsKeyMatch).ToList();
                 }
+
+                retcon = sortCon;
 
                 if (cats.Count > 0)
                 {
+
+
                     var retcon2 = new Dictionary<string, List<SearchResult>>();
 
                     foreach (var i in retcon.Where(i => cats.Contains(i.Key.ToLower())))
@@ -99,13 +107,11 @@ namespace Nyan.Modules.Web.Tools.Search
 
                 step = "Returning content";
 
-
                 var finalCon = new Dictionary<string, object>();
                 var regCount = 0;
 
                 foreach (var i in retcon)
                 {
-
                     foreach (var j in i.Value)
                     {
                         j.Body = j.Body?.StripHtml();
@@ -119,40 +125,34 @@ namespace Nyan.Modules.Web.Tools.Search
                 finalCon.Add("_total", regCount);
 
                 return finalCon;
-
-            }
-            catch (Exception e)
+            } catch (Exception e)
             {
                 Current.Log.Add("Error while " + step + ":", Message.EContentType.Warning);
                 Current.Log.Add(e);
-                return new Dictionary<string, object> { { "_total", 0 } };
+                return new Dictionary<string, object> {{"_total", 0}};
             }
         }
 
         private static KeyValuePair<string, SearchResultBlock> ProcessSearchableType(ISearch instance, string term)
         {
-            var ret = new SearchResultBlock { Results = new List<SearchResult>() };
+            var ret = new SearchResultBlock {Results = new List<SearchResult>()};
 
             var sw = new Stopwatch();
 
             sw.Start();
 
-
-            try { ret.Results = instance.SimpleQuery(term); }
-            catch (Exception e) { Current.Log.Add(e); }
+            try { ret.Results = instance.SimpleQuery(term); } catch (Exception e) { Current.Log.Add(e); }
 
             sw.Stop();
             ret.ProcessingTime = sw.ElapsedMilliseconds;
 
             return new KeyValuePair<string, SearchResultBlock>(instance.SearchResultMoniker, ret);
-
         }
 
         internal class SearchResultBlock
         {
-            internal List<SearchResult> Results = new List<SearchResult>();
             internal long ProcessingTime;
-
+            internal List<SearchResult> Results = new List<SearchResult>();
         }
     }
 }
