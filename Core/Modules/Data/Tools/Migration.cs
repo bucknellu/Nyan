2b573414d469c19f0a83af59c21bdc1ebddb7a53
Leo Botinelly <lmbs001@bucknell.edu>
2019-02-06 12:13:32 -0500

    Data Migration handler

---
+++ Core/Modules/Data/Tools/Migration.cs
@@ -0,0 +1,16 @@
+﻿using Nyan.Core.Assembly;
+using Nyan.Core.Extensions;
+using Nyan.Core.Modules.Data.Contracts;
+
+namespace Nyan.Core.Modules.Data.Tools
+{
+    public static class Migration
+    {
+        public static void RunAll()
+        {
+            var allTypes = Management.GetClassesByInterface<IMigration>().ToInstances<IMigration>();
+
+            foreach (var migration in allTypes) migration.MigrateStart();
+        }
+    }
+}
