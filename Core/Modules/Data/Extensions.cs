effa6c61ae580e5f6ce23bb647fa82ac852bb7c0
Leo Botinelly <lmbs001@bucknell.edu>
2019-02-13 15:49:48 -0500

    Reference handling for Document storage

--- Core/Modules/Data/Extensions.cs
+++ Core/Modules/Data/Extensions.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
+using Nyan.Core.Modules.Data.Contracts;
 
 namespace Nyan.Core.Modules.Data
 {
     public static class Extensions
     {
+
+        public static EntityReference ToReference<T>(this MicroEntity<T> source) where T : MicroEntity<T> => new EntityReference { Id = source.GetEntityIdentifier(), Label = source.GetEntityLabel() };
+
+        public static T ToEntity<T>(this EntityReference source) where T : MicroEntity<T> => MicroEntity<T>.Get(source.Id);
+
+        public static List<T> ToEntityList<T>(this IEnumerable<EntityReference> source) where T : MicroEntity<T>
+        {
+            var entityReferences = source.ToList();
+            return MicroEntity<T>.Get(entityReferences.Select(i => i.Id).Where(i => i != null)).ToList();
+        }
+
         public static void RemoveAll<T>(this List<T> sourceList) where T : MicroEntity<T>
         {
             Parallel.ForEach(sourceList, new ParallelOptions { MaxDegreeOfParallelism = 10 }, i => { i.Remove(); });
