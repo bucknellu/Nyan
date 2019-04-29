22eed35a2af52baf0bf8e350494f998c3583952c
Leo Botinelly <lmbs001@bucknell.edu>
2019-04-29 16:02:37 -0400

    Heartbeat diagnostics endpoint

---
+++ Modules/Web/REST/Diagnostics/StackToolsDiagnosticsController.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using Nyan.Core.Modules.Diagnostics;
+
+namespace Nyan.Modules.Web.REST.Diagnostics
+{
+    [RoutePrefix("stack/diagnostics")]
+    public class StackToolsDiagnosticsController : ApiController
+    {
+        #region HTTP Methods
+
+        [Route("heartbeat/{category}"), HttpGet]
+        public virtual HttpResponseMessage StackToolsDiagnosticsRunByCategory(string category) { return Factory.RunDiagnostics(category).AsResponse(); }
+
+        [Route("heartbeat"), HttpGet]
+        public virtual HttpResponseMessage StackToolsDiagnosticsRun() { return Factory.RunDiagnostics().AsResponse(); }
+
+        #endregion
+    }
+}
