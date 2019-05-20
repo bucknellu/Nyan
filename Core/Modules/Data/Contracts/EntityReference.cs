ff6da4d74524402f83aab503246c60560385e485
Leo Botinelly <lmbs001@bucknell.edu>
2019-05-20 08:51:58 -0400

    Some standard constructors for EntityReference

--- Core/Modules/Data/Contracts/EntityReference.cs
+++ Core/Modules/Data/Contracts/EntityReference.cs
@@ -3,5 +3,17 @@
     {
         public string Id;
         public string Label;
+        public EntityReference() { }
+
+        public EntityReference(string id, string label)
+        {
+            Id = id;
+            Label = label;
+        }
+        public EntityReference(long id, string label)
+        {
+            Id = id.ToString();
+            Label = label;
+        }
     }
 }
