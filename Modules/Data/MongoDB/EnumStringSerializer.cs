43f3a22d59d76f89de6642486db122a41cd3535c
Daniel  Mancusi <dmancusi@bucknell.edu>
2024-02-09 14:46:07 -0500

    Mongo Driver update

--- Modules/Data/MongoDB/EnumStringSerializer.cs
+++ Modules/Data/MongoDB/EnumStringSerializer.cs
@@ -1,9 +1,10 @@
-﻿using MongoDB.Bson;
+﻿using System;
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Serializers;
 
 namespace Nyan.Modules.Data.MongoDB
 {
-    public class EnumStringSerializer<TEnum> : EnumSerializer<TEnum> where TEnum : struct
+    public class EnumStringSerializer<TEnum> : EnumSerializer<TEnum> where TEnum : struct, Enum
     {
         public EnumStringSerializer() : base(BsonType.String) { }
     }
